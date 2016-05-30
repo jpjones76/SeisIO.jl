b98e98afc0525d4aee1ab05c6b7778d42adafc9b
Joshua Jones <highly.creative.pseudonym@gmail.com>
2016-05-30 01:53:59 -0700

    batch_read types bugfix

--- src/FileFormats/BatchProc.jl
+++ src/FileFormats/BatchProc.jl
@@ -18,7 +18,7 @@ FILESTR supports wildcard filenames, but not directories. Thus,
 
 Batch read and resample to FS Hz.
 """
-function batch_read(files::Array{ByteString,1}; ftype=ftype::ASCIIString, fs=0.0::Float64)
+function batch_read(files::Array{ByteString,1}; ftype="SAC"::ASCIIString, fs=0.0::Float64)
   file0 = files[1]
   files = files[2:end]
   NF = length(files)
@@ -44,19 +44,19 @@ function batch_read(files::Array{ByteString,1}; ftype=ftype::ASCIIString, fs=0.0
   dt = 1/fs
 
   # Parallelize file read into a shared array
-  DT = collect(repeated(round(Int, dt*1.0e6),NF))
+  DT = Array{Int64,1}(collect(repeated(round(Int, dt*1.0e6),NF)))
   ts = Array{Int64,1}(NF)
   nx = Array{Int64,1}(NF)
   rr = Array{Float64,1}(NF)
-  gain = Array{Float32,1}(NF)
+  gain = Array{Float64,1}(NF)
 
   if ftype == "SAC"
-    fmt = collect(repeated(Float32, NF))
+    fmt = Array{Type,1}(collect(repeated(Float32, NF)))
     for i = 1:NF
       (ts[i], nx[i], gain[i], rr[i]) = sac_geth(files[i])
     end
     os = 632
-  elseif ftype == "SEGY"
+  elseif ftype == "NMT" || ftype == "PASSCAL"
     fmt = Array{Type,1}(NF)
     for i = 1:NF
       (ts[i], nx[i], fmt[i], gain[i], rr[i]) = segy_geth(files[i])
@@ -65,11 +65,11 @@ function batch_read(files::Array{ByteString,1}; ftype=ftype::ASCIIString, fs=0.0
   end
 
   # Indices and counters
-  OS = collect(repeated(os, NF))
+  OS = Array{Int64,1}(collect(repeated(os, NF)))
   rr ./= dt
   lx = length(S.x)
   NN = ceil(Int, nx.*rr)
-  ei = lx .+ cumsum(NN)
+  ei = lx + cumsum(NN)
   si = [lx; ei[1:end-1]]
   ll = ei[end]
 
@@ -86,8 +86,9 @@ function batch_read(files::Array{ByteString,1}; ftype=ftype::ASCIIString, fs=0.0
     DT, OS, collect(repeated(tt, NF)), collect(repeated(xx, NF)))
 
   # Rearrange and prune T, X
-  i = sortperm(sdata(tt))
-  t1 = sdata(tt)[i]
+  t1 = sdata(tt)
+  i = sortperm(t1)
+  t1 = t1[i]
   x1 = sdata(xx)[i]
   j = find(t1.==0)
   if !isempty(j)
@@ -105,7 +106,7 @@ end
 batch_read(filestr::ASCIIString; ftype=ftype::ASCIIString, fs=0.0::Float64) = batch_read(lsw(filestr), ftype=ftype, fs = fs)
 # =============================================================================
 
-function segy_geth(fname)
+function segy_geth(fname::ByteString)
   f = open(fname, "r")
   seek(f, 102)
   vals = read(f, Int16, 9)
@@ -121,39 +122,41 @@ function segy_geth(fname)
   skip(f, 4)
   npts = read(f, Int32)
   close(f)
-  nx = vals[7] == 32767 ? npts : vals[7]                                  # nx
-  gain = (scale_fac > 0 && vals[9] > 0) ? (scale_fac / vals[9]) : 1.0     # gain
-  dt = Float64((vals[8] == 1 ? samp_rate : vals[8])*1.0e-6)               # fs
+  nx = Int64(vals[7] == 32767 ? npts : vals[7])                             # nx
+  g = Float64((scale_fac > 0 && vals[9] > 0) ? (scale_fac / vals[9]) : 1.0) # gain
+  dt = Float64((vals[8] == 1 ? samp_rate : vals[8])*1.0e-6)                 # fs
   (m,d) = j2md(ti[1],ti[2])
   t = round(Int, d2u(DateTime(ti[1],m,d,ti[3],ti[4],ti[5]))*1.0e6
-    + (ms+sum(vals[1:4]))*1000)                                           # t
-  return (t, nx, fmt, gain, dt)
+    + (ms+sum(vals[1:4]))*1000)                                             # t
+  return (t, nx, fmt, g, dt)
 end
 # =============================================================================
 
-function read_bat_1(fname, nx, rr, i, fmt, gain, ts, dt, os, tt, xx)
+function sac_geth(fname::ByteString)
+  f = open(fname, "r")
+  fv = read(f, Float32, 70)
+  iv = read(f, Int32, 16)
+  g = Float64(fv[4] == -12345.0f0 ? 1.0 : fv[4])
+  (m,d) = j2md(iv[1],iv[2])
+  t = round(Int, d2u(DateTime(iv[1],m,d,iv[3],iv[4],iv[5]))*1.0e6
+    + (iv[6]*1000) + (fv[6] == -12345.0 ? 0.0 : fv[6])*1000)
+  close(f)
+  return (t, Int64(iv[10]), g, Float64(fv[1]))
+end
+
+# =============================================================================
+function read_bat_1(fname::ByteString, nx::Int64, rr::Float64, i::Int64,
+  fmt::Type, gain::Float64, ts::Int64, dt::Int64, os::Int64, tt::SharedArray{Int64,1},
+  xx::SharedArray)
   f = open(fname,"r")
   seek(f, os)
-  x = read(f, fmt, nx).*gain
+  x = map(Float64, read(f, fmt, nx)).*gain
   close(f)
   if !isapprox(rr, 1.0)
     x = resample(x, rr)
   end
   lx = length(x)
   xx[i+1:i+lx] = x
-  tt[i+1:i+lx] = ts + cumsum(collect(repeated(dt, lx)))
+  tt[i+1:i+lx] = cumsum([ts; collect(repeated(dt, lx-1))])
   return
-end
-
-# =============================================================================
-function sac_geth(fname)
-  f = open(fname, "r")
-  fv = read(f, Float32, 70)
-  iv = read(f, Int32, 16)
-  g = fv[4] == -12345.0f0 ? 1.0 : fv[4]
-  (m,d) = j2md(iv[1],iv[2])
-  t = round(Int, d2u(DateTime(iv[1],m,d,iv[3],iv[4],iv[5]))*1.0e6
-    + (iv[6]*1000) + (fv[6] == -12345.0 ? 0.0 : fv[6])*1000)
-  close(f)
-  return (t, iv[10], g, fv[1])
 end
