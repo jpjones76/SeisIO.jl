e6b9ac8cc5fbe4910eb2b842eb890cf81c39ad00
Joshua Jones <jpjones76@users.noreply.github.com>
2018-12-20 01:52:42 -0800

    HOOD is either offline or not on IRISws right now

--- test/test_iris.jl
+++ test/test_iris.jl
@@ -13,7 +13,8 @@ sync!(T, s=ts, t=te)
 @test ≈(S.x[1], T.x[1])
 
 println("...IRISget...")
-chans = ["UW.TDH..EHZ", "UW.VLL..EHZ", "UW.HOOD..BHZ"]
+#chans = ["UW.TDH..EHZ", "UW.VLL..EHZ", "UW.HOOD..BHZ"]
+chans = ["UW.TDH..EHZ", "UW.VLL..EHZ"] # HOOD is either offline or not on IRISws right now
 st = -86400.0
 en = -86100.0
 (d0,d1) = parsetimewin(st,en)
