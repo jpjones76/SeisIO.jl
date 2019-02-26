52086769ebafadffdacbcf7c820fde8efa508f25
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-02-26 12:22:45 -0800

    restructured tests/

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,17 @@
 using Test, Compat, Dates, Random, SeisIO
-
 path = Base.source_dir()
 @warn("Tests require up to 20 minutes to execute. Begin in 5 seconds...")
-sleep(5)
-printstyled(stdout, string(Dates.now(), ": tests begin, source_dir = ", path, "\n"), color=:light_green, bold=true)
-
 include("test_helpers.jl")
+sleep(5)
+printstyled(stdout, string(Dates.now(), ": tests begin, source_dir = ", path, "/\n"), color=:light_green, bold=true)
 
-# Utilities that don't require SeisIO types to work
-printstyled("Testing CoreUtils\n", color=:light_green, bold=true)
-for i in readdir(path*"/CoreUtils")
-  include(joinpath("CoreUtils",i))
+# huehuehue grep "include(joinpath" runtests.jl | awk -F "(" '{print $3}' | awk -F "," {'print $1'}
+for d in ["CoreUtils", "Types", "NativeIO", "DataFormats", "Web"]
+  printstyled(string("Testing ", d, "/\n"), color=:light_green, bold=true)
+  for i in readdir(path*"/"*d)
+    include(joinpath(d,i))
+  end
 end
 
-# Utilities that don't require SeisIO types to work
-printstyled("Testing Types\n", color=:light_green, bold=true)
-for i in readdir(path*"/Types")
-  include(joinpath("Types",i))
-end
-
-# Utilities that don't require SeisIO types to work
-printstyled("Testing File IO\n", color=:light_green, bold=true)
-for i in readdir(path*"/FileIO")
-  include(joinpath("FileIO",i))
-end
-
-# Utilities that don't require SeisIO types to work
-printstyled("Testing Web Functionality\n", color=:light_green, bold=true)
-for i in readdir(path*"/Web")
-  include(joinpath("Web",i))
-end
-
-printstyled("...done!\n", color=:light_green, bold=true)
-
-# printstyled("To run canonical examples, execute this command: include(\"", path, "/examples.jl\")")
+printstyled("Done!\n", color=:light_green, bold=true)
+printstyled("To run canonical examples, execute this command: include(\"", path, "/examples.jl\")", color=:cyan, bold=true)
