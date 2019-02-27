e3bd879988120acd705a16f7803a3f30ef43e21d
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-02-27 05:18:51 -0800

    method deprecation for "-"

--- test/Types/custom_types_3.jl
+++ test/Types/custom_types_3.jl
@@ -2,3 +2,18 @@ H = randSeisHdr()
 @test sizeof(H) > 0
 clear_notes!(H)
 @test length(H.notes) == 0
+
+(S,T) = mktestseis()
+U = S-T
+sizetest(S,5)
+sizetest(T,4)
+sizetest(U,3)
+
+(S,T) = mktestseis()
+@test (S + T - T) == S
+
+(S,T) = mktestseis()
+U = deepcopy(S)
+deleteat!(U, 1:3)
+@test (S - [1,2,3]) == U
+sizetest(S,5)
