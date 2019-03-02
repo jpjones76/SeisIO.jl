40956d6073103627e7cc2b24256529d45853165b
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-03-02 14:28:18 -0800

    moved time blockette parse to mSEEDblk.jl
    Former-commit-id: 7256c61708994e71c0a1d566052804be1d38c941
    Former-commit-id: 3460af0fd0c115faceee2c8d9004f6377dd9ca11

--- src/Formats/mSEED.jl
+++ src/Formats/mSEED.jl
@@ -15,25 +15,6 @@ function unpack!(S::SeedVol)
   return nothing
 end
 
-function blk_time!(t::Array{Int32,1}, sid::IOStream, b::Bool)
-  yy    = read(sid, UInt16)
-  jj    = read(sid, UInt16)
-  t[4]  = Int32(read(sid, UInt8))
-  t[5]  = Int32(read(sid, UInt8))
-  t[6]  = Int32(read(sid, UInt8))
-  skip(sid, 1)
-  ms    = read(sid, UInt16, 1)
-  if b
-    yy = bswap(yy)
-    jj = bswap(jj)
-    ms = bswap(ms)
-  end
-  t[1] = Int32(yy)
-  (t[2], t[3]) = j2md(t[1], Int32(jj))
-  t[7] = Int32(ms)*Int32(100)
-  return nothing
-end
-
 ###############################################################################
 function parserec!(S::SeisData, sid::IO, v::Int)
   # =========================================================================
