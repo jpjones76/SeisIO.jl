449286404b2da9a7ec2fe8b7f8bf95e19c1a13d7
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-03-02 02:56:15 -0800

    test bugfix
    Former-commit-id: 3c7e3111715c97a5bbb1c820454e1b4768197ddd
    Former-commit-id: efdc4bdd6b59f8ea29ff1b52980ee3173a89e09c

--- test/RandSeis/test_randseis.jl
+++ test/RandSeis/test_randseis.jl
@@ -1,20 +1,19 @@
-import SeisIO.RandSeis: pop_rand_dict!, getyp2codes
 printstyled("  RandSeis utils...\n", color=:light_green)
 
 # This should do it
 D = Dict{String,Any}()
-pop_rand_dict!(D, 1000)
+SeisIO.RandSeis.pop_rand_dict!(D, 1000)
 
 # Similarly for the yp2 codes
-for i = 1:1000
-  i,c,u = getyp2codes('s', true)
+for i = 1:100
+  i,c,u = SeisIO.RandSeis.getyp2codes('s', true)
   @test isa(i, Char)
   @test isa(c, Char)
   @test isa(u, String)
 end
 
 for i = 1:10000
-  i,c,u = getyp2codes('s', false)
+  i,c,u = SeisIO.RandSeis.getyp2codes('s', false)
   @test isa(i, Char)
   @test isa(c, Char)
   @test isa(u, String)
