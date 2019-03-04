c7be131c3a6b4c7c6f78fcbe80c6750d51b6f1d5
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-03-04 03:35:37 -0800

    update SeedLink.jl

--- test/Web/test_seedlink.jl
+++ test/Web/test_seedlink.jl
@@ -1,36 +1,54 @@
+import SeisIO: parse_charr, parse_chstr, parse_sl
+
 # Seedlink with command-line stations
-sta = ["CC.SEP", "UW.HDW"]
 config_file = path*"/SampleFiles/seedlink.conf"
+sta = ["CC.SEP", "UW.HDW"]
+pat = ["?????.D"; "?????.D"]
+trl = ".??.???.D"
 
 printstyled("SeedLink\n", color=:light_green, bold=true)
+printstyled("  (test requires 5 minutes)\n", color=:green)
 
-# Checking
+# has_stream
 printstyled("  has_stream...\n", color=:light_green)
-tf = has_stream(sta, u="rtserve.iris.washington.edu")
+tf1 = has_stream(sta, u="rtserve.iris.washington.edu")
+tf2 = has_stream(sta, pat, u="rtserve.iris.washington.edu", d='.')
+@test tf1 == tf2
 
+# has_stream
 printstyled("  has_sta...\n", color=:light_green)
-tf = has_sta(sta[1], u="rtserve.iris.washington.edu")
+tf1 = has_sta(sta[1], u="rtserve.iris.washington.edu")[1]
+tf2 = has_sta(sta[1]*trl, u="rtserve.iris.washington.edu")[1]
+tf3 = has_sta(sta, u="rtserve.iris.washington.edu")[1]
+tf4 = has_sta(parse_charr(sta), u="rtserve.iris.washington.edu")[1]
+@test tf1 == tf2 == tf3 == tf4
 
-# SeedLink DATA mode (easier)
-printstyled("  DATA mode...\n", color=:light_green)
+# DATA mode
+printstyled("  SeedLink DATA mode...\n", color=:light_green)
 T = SeisData()
+
 printstyled("    ...link 1: command-line station list...\n", color=:light_green)
-SeedLink!(T, sta, mode="DATA", refresh=11.1)
+SeedLink!(T, sta, mode="DATA", refresh=13.1, kai=90.0)
+
 printstyled("    ...link 2: station file...\n", color=:light_green)
-SeedLink!(T, config_file, mode="DATA", refresh=13.3)
-printstyled("    ...sleep 60.0s while SeedLink session receives data...\n", color=:green)
-sleep(60.0)
-for i = 1:length(T.c)
-  close(T.c[i])
+printstyled("      (sleep 90.0 s)\n", color=:green)
+open("show.log", "w") do out
+  redirect_stdout(out) do
+    SeedLink!(T, config_file, mode="DATA", refresh=9.9, v=3)
+
+    sleep(60.0)
+    for i = 1:length(T.c)
+      close(T.c[i])
+    end
+
+    sleep(30.0)
+  end
 end
-printstyled("    ...sleep 60.0s while SeedLink session closes...\n", color=:green)
-sleep(60.0)
 @test isempty(T)==false
-printstyled("  ...moving on now.\n", color=:light_green)
 sync!(T, s="first")
 
 # SeedLink time mode (more complicated)
-printstyled("  TIME mode...\n", color=:light_green)
+printstyled("  SeedLink TIME mode...\n", color=:light_green)
 
 # To ensure precise timing, we'll pass d0 and d1 as strings
 st = 0.0
@@ -39,7 +57,7 @@ dt = en-st
 (d0,d1) = parsetimewin(st,en)
 
 S = SeisData()
-SeedLink!(S, sta, mode="TIME", refresh=10.0, s=d0, t=d1, v=0)
+SeedLink!(S, sta, mode="TIME", refresh=10.0, s=d0, t=d1, w=true)
 printstyled("    ...first link initialized...\n", color=:light_green)
 
 # Seedlink with a config file
@@ -49,19 +67,47 @@ printstyled("    ...second link initialized...\n", color=:light_green)
 # Seedlink with a config string
 SeedLink!(S, "CC.VALT..???, UW.ELK..EHZ", mode="TIME", refresh=10.0, s=d0, t=d1)
 printstyled("    ...third link initialized...\n", color=:light_green)
+printstyled(string("      (sleep ", dt + 30.0, " s)\n"), color=:green)
 
 # This takes time
-printstyled(string("    ...sleep ", dt, " s while SeedLink session receives data...\n"), color=:green)
 sleep(dt)
-for i = 1:length(S.c)
-    i == 3 && show(S)
-    close(S.c[i])
+open("show.log", "w") do out
+  redirect_stdout(out) do
+    for i = 1:length(S.c)
+        i == 3 && show(S)
+        close(S.c[i])
+    end
+  end
 end
-
 @test isempty(S)==false
 
 # Synchronize (the reason we used d0,d1 above)
-printstyled(string("    ...sleep ", dt, " s while SeedLink closes...\n"), color=:green)
-sleep(dt)
-printstyled("  ...moving on now.\n", color=:light_green)
+sleep(30)
 sync!(S, s="first", t="last")
+
+printstyled("  SeedLink FETCH mode...\n", color=:light_green)
+
+# To ensure precise timing, we'll pass d0 and d1 as strings
+S = SeisData()
+SeedLink!(S, config_file, refresh=10.0, mode="FETCH", s=d0, t=d1)
+printstyled("    ...link initialized...\n", color=:light_green)
+printstyled(string("      (sleep ", dt + 20.0, " s)\n"), color=:green)
+sleep(dt)
+close(S.c[1])
+sleep(20)
+
+# Attempting to produce errors
+printstyled("  Checking that errors and warnings are written correctly...\n", color=:light_green)
+S = SeisData()
+pat = ["*****.X"]
+@test_throws ErrorException SeedLink!(S, [sta[1]], pat)
+
+pat = ["?????.D"]
+@test_throws ErrorException SeedLink!(S, [replace(sta[1], "SEP" => "XOX")], pat)
+
+# Done. Clean up.
+rm("show.log")
+files = ls("*.mseed")
+for f in files
+  rm(f)
+end
