c8ce11b78b47a383377c33569bba39c87930fe48
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-03-10 22:26:00 -0700

    test for bigEndian SAC read
    Former-commit-id: 1de16e955a1937a5344c43e3e318559a36821c95
    Former-commit-id: a0edeb82552ce3f82f525192660da3d14674c1de

--- test/DataFormats/test_sac.jl
+++ test/DataFormats/test_sac.jl
@@ -1,6 +1,6 @@
-# test.sac was generated in SAC 101.6a with "fg seismogram; write test.sac"
-sac_file = path*"/SampleFiles/test.sac"
-sac_be_file = path*"/SampleFiles/060123130000.CON.1.sac"
+# test_le.sac was generated in SAC 101.6a with "fg seismogram; write test_le.sac"
+sac_file = path*"/SampleFiles/test_le.sac"
+sac_be_file = path*"/SampleFiles/test_be.sac"
 
 printstyled("  SAC\n", color=:light_green)
 printstyled("    read\n", color=:light_green)
@@ -19,7 +19,7 @@ SAC3 = readsac(sac_be_file, full=true)
 @test ≈(1/SAC3.fs, SAC3.misc["delta"])
 @test ≈(length(SAC3.x), SAC3.misc["npts"])
 
-open("show.log", "a") do out
+open("runtests.log", "a") do out
   redirect_stdout(out) do
     sachdr(sac_be_file)
   end
@@ -35,7 +35,7 @@ SAC1.name = "VU.CDV..NUL"
 writesac(SAC1)
 @test SeisIO.safe_isfile("1981.088.10.38.14.009.VU.CDV..NUL.R.SAC")
 
-open("show.log", "a") do out
+open("runtests.log", "a") do out
   redirect_stdout(out) do
     writesac(SAC1, ts=true, v=1)
   end
