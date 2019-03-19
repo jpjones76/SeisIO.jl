b0ee045575c6e9ecdbea839addc4ef5a7a7b3054
Joshua Jones <highly.creative.pseudonym@gmail.com>
2019-03-18 21:29:10 -0700

    fixed support for keyword v

--- src/Processing/Merge/merge_ext.jl
+++ src/Processing/Merge/merge_ext.jl
@@ -1,7 +1,7 @@
 # Home of all extended merge! methods
-merge(S::SeisData) = (U = deepcopy(S); merge!(U); return U)
-merge!(S::SeisData, U::SeisData) = ([append!(getfield(S, f), getfield(U, f)) for f in SeisIO.datafields]; S.n += U.n; merge!(S))
-merge!(S::SeisData, C::SeisChannel) = merge!(S, SeisData(C))
+merge(S::SeisData; v::Int64=KW.v) = (U = deepcopy(S); merge!(U, v=v); return U)
+merge!(S::SeisData, U::SeisData; v::Int64=KW.v) = ([append!(getfield(S, f), getfield(U, f)) for f in SeisIO.datafields]; S.n += U.n; merge!(S; v=v))
+merge!(S::SeisData, C::SeisChannel; v::Int64=KW.v) = merge!(S, SeisData(C), v=v)
 
 """
     S = merge(A::Array{SeisData,1})
@@ -11,17 +11,17 @@ input data.
 
 See also: merge!
 """
-function merge(A::Array{SeisIO.SeisData,1})
+function merge(A::Array{SeisIO.SeisData,1}; v::Int64=KW.v)
   L::Int64 = length(A)
   n = sum([A[i].n for i = 1:L])
   T = SeisData(n)
   [setfield!(T, f, vcat([getfield(A[i],f) for i = 1:L]...)) for f in SeisIO.datafields]
-  return merge!(T)
+  return merge!(T, v=v)
 end
-merge(S::SeisData, U::SeisData) = merge(Array{SeisData,1}([S,U]))
-merge(S::SeisData, C::SeisChannel) = merge(S, SeisData(C))
-merge(C::SeisChannel, S::SeisData) = merge(SeisData(C), S)
-merge(C::SeisChannel, D::SeisChannel) = (S = SeisData(C,D); merge!(S))
+merge(S::SeisData, U::SeisData; v::Int64=KW.v) = merge(Array{SeisData,1}([S,U]), v=v)
+merge(S::SeisData, C::SeisChannel; v::Int64=KW.v) = merge(S, SeisData(C), v=v)
+merge(C::SeisChannel, S::SeisData; v::Int64=KW.v) = merge(SeisData(C), S, v=v)
+merge(C::SeisChannel, D::SeisChannel; v::Int64=KW.v) = (S = SeisData(C,D); merge!(S, v=v))
 
 # The "*" operator for SeisData
 *(S::SeisData, U::SeisData) = merge(Array{SeisData,1}([S,U]))
