4dd13febbddd7fcb1377f9fa4020686d2a037fcc
Joshua Jones <jpjones76@users.noreply.github.com>
2019-04-24 01:50:49 -0700

    added KWs nx_new, nx_add; performance improvements

--- src/Formats/SEED/readmseed.jl
+++ src/Formats/SEED/readmseed.jl
@@ -1,32 +1,47 @@
 export readmseed, readmseed!
 
-function parsemseed!(S::SeisData, sid::IO, v::Int)
-  while !eof(sid)
-    parserec!(S, sid, v)
-  end
+function seed_cleanup!(S::SeisData, SEED::SeedVol)
   trunc_x!(S)
+  fill!(getfield(SEED, :hdr_old), zero(UInt8))
+  setfield!(SEED, :r1_old, zero(Int16))
+  setfield!(SEED, :r2_old, zero(Int16))
+  return nothing
+end
+
+function parsemseed!(S::SeisData, sid::IO, v::Int64, nx_new::Int64, nx_add::Int64)
+  while !eof(sid)
+    parserec!(S, SEED, sid, v, nx_new, nx_add)
+  end
+  seed_cleanup!(S, SEED)
   return S
 end
 
 """
-    S = readmseed(fname)
+    readmseed(fname[, KWs])
 
-Read file S into a SeisData structure.
+Read mini-SEED file `fname` into a new SeisData structure.
 
-  readmseed!(S, fname)
+  readmseed!(S, fname[, KWs])
 
-Read `fname` into `S`.
+Read mini-SEED file `fname` into existing SeisData structure `S`.
 
-Note: this is a mini-SEED reader,. not a full SEED reader. Currently supported
-SEED blockette types include [100], [201], [500], [1000], [1001].
-
-Keywords:
+### Keywords
 * swap: Byte swap. Set false for no (little-Endian). readmseed should be able
 to determinme whether to byte swap automatically; only use this keyword if you
 encounter errors. (Default: false)
 * v: Verbosity. (Default: 0)
+* nx_new: allocate data arrays of length `N` samples for each new channel.
+(Default: 86400000) [^a]
+* nx_add: increase `S.x[i]` by at least `N` samples when new data are added to
+channel `i`. (Default: 360000) [^a]
+
+[^a]: After data read, unused memory is freed by resizing arrays in S.x; thus, for best performance, set nx_new ≥ the longest expected time-series length in samples.
 """
-function readmseed!(S::SeisData, fname::String; swap=false::Bool, v::Int=KW.v)
+function readmseed!(S::SeisData, fname::String;
+                    swap=false::Bool,
+                    v::Int=KW.v,
+                    nx_new::Int64=KW.nx_new,
+                    nx_add::Int64=KW.nx_add)
   setfield!(SEED, :swap, swap)
 
   if safe_isfile(fname)
@@ -34,11 +49,18 @@ function readmseed!(S::SeisData, fname::String; swap=false::Bool, v::Int=KW.v)
     skip(fid, 6)
     (findfirst(isequal(read(fid, Char)), "DRMQ") > 0) || error("Scan failed due to invalid file type")
     seek(fid, 0)
-    parsemseed!(S, fid, v)
+    parsemseed!(S, fid, v, nx_new, nx_add)
     close(fid)
   else
     error("Invalid file name!")
   end
   return nothing
 end
-readmseed(fname::String; swap=false::Bool, v::Int=KW.v) = (S = SeisData(); readmseed!(S, fname, swap=swap, v=v); return S)
+readmseed(fname::String;
+          swap=false::Bool,
+          v::Int=KW.v,
+          nx_new::Int64=KW.nx_new,
+          nx_add::Int64=KW.nx_add
+          ) = (S = SeisData();
+               readmseed!(S, fname, swap=swap, v=v, nx_new=nx_new, nx_add=nx_add);
+               return S)
--- src/Web/1_IRIS.jl
+++ src/Web/1_IRIS.jl
@@ -42,7 +42,7 @@ function irisws(cha::String, d0::String, d1::String;
         Ch.name = deepcopy(Ch.id)
       end
     elseif fmt == "miniseed"
-      parsemseed!(S, IOBuffer(R), v)
+      parsemseed!(S, IOBuffer(R), v, KW.nx_add, KW.nx_add)
       Ch = S[1]
       if isempty(Ch.loc)
         Ch.loc = zeros(Float64,5)
