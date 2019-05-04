564e3db1ebf4de8053f51d731e1117567cb63315
Joshua <jpjones76@users.noreply.github.com>
2019-05-04 04:48:20 -0700

    test improvements
    Former-commit-id: 130cdfee0461882f4d54034618a015140f234bd0
    Former-commit-id: 3b60a8e69a2b83f04d27ed55c60b09a69b1403c4
    Former-commit-id: 0f90ec15d9133aaf0bfdfb701eae9763a786f35f

--- test/DataFormats/test_mseed.jl
+++ test/DataFormats/test_mseed.jl
@@ -3,7 +3,7 @@
 # versions of libmseed, which reads with no issues
 printstyled("  mini-SEED file read\n", color=:light_green)
 
-@test_throws ErrorException readmseed("/data/Code/SeisIO/test/SampleFiles/one_day.sac")
+@test_throws ErrorException readmseed(string(path, "/SampleFiles/one_day.sac"))
 
 S = readmseed(string(path, "/SampleFiles/test.mseed"), v=0)
 @test isequal(S.id[1], "NL.HGN.00.BHZ")
