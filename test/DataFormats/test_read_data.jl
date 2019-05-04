23d1a901e70868524779a0e2dee07a0f02917cc7
Joshua <jpjones76@users.noreply.github.com>
2019-05-03 21:10:25 -0700

    test improvements
    Former-commit-id: 3044d9ea4bec3b63d8a387e5a64c0fa497f7e2ed
    Former-commit-id: 39fbe7359662dd208c7aed0d612d2ad100feea62
    Former-commit-id: 30638f54e615f965b7cc032c351cdb4c47c22f12

--- test/DataFormats/test_read_data.jl
+++ test/DataFormats/test_read_data.jl
@@ -1,5 +1,4 @@
 import SeisIO:safe_isfile
-printstyled("  read_data\n", color=:light_green)
 nx_add = 1400000
 nx_new = 36000
 pref = path * "/SampleFiles/"
@@ -7,7 +6,7 @@ cfile = pref * "Restricted/03_02_27_20140927.sjis.ch"
 files = String[ "99011116541W"                "uw"            "_"
                 "test_PASSCAL.segy"           "passcal"       "pa-full"
                 "test_PASSCAL.segy"           "passcal"       "passcal"
-                "one_dasfgay.sac"                 "sac"           "_"
+                "one_day.sac"                 "sac"           "_"
                 "one_day.mseed"               "mseed"         "_"
                 "Restricted/2014092709*.cnt"  "win32"         "win"
                 "0215162000.c00"              "lennasc"       "_"
@@ -19,26 +18,26 @@ files = String[ "99011116541W"                "uw"            "_"
                 "test_be.sac"                 "sac"           "_"
                 "FDSNWS.IRIS.geocsv"          "geocsv"        "_"      ]
 
-for n = 1:size(files,1)
-  fname = pref * files[n,1]
-  if safe_isfile(fname) == false
-    continue
-  else
+# Do not run on Appveyor; it can't access restricted files, so this breaks
+if Sys.iswindows() == false
+  printstyled("  read_data\n", color=:light_green)
+  for n = 1:size(files,1)
+    fname = pref * files[n,1]
     f_call = files[n,2]
     opt = files[n,3]
     printstyled(string("    ", f_call, "\n"), color=:light_green)
-  end
-  if opt == "pa-full"
-    S = read_data("passcal", fname, full=true)
-  elseif opt == "win"
-    S = read_data(f_call, fname, cf=cfile)
-  elseif opt == "slist"
-    S = read_data("geocsv.slist", fname, tspair=false)
-  elseif opt == "lo-mem"
-    S = read_data(f_call, fname, nx_new=nx_new, nx_add=nx_add)
-  elseif opt == "full"
-    S = read_data(f_call, fname, full=true)
-  else
-    S = read_data(f_call, fname)
+    if opt == "pa-full"
+      S = read_data("passcal", fname, full=true)
+    elseif opt == "win"
+      S = read_data(f_call, fname, cf=cfile)
+    elseif opt == "slist"
+      S = read_data("geocsv.slist", fname, tspair=false)
+    elseif opt == "lo-mem"
+      S = read_data(f_call, fname, nx_new=nx_new, nx_add=nx_add)
+    elseif opt == "full"
+      S = read_data(f_call, fname, full=true)
+    else
+      S = read_data(f_call, fname)
+    end
   end
 end
