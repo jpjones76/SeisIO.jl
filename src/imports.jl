a718c8fb60312ca124ff59d57562214a3e066360
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 03:15:12 -0700

    consolidated "import" statements
    Former-commit-id: dd2ec184b5c6aa6c72bfa158801dc444f25a8c63
    Former-commit-id: 0d82744a31c31979d75db8bfe289fbe7b15edd86
    Former-commit-id: 50285e9d683f2c0b0de4e16f77732618eb63c8de

---
+++ src/imports.jl
@@ -0,0 +1,5 @@
+import Base: ==, -, *, +, append!, convert, copy, delete!, deleteat!,
+  firstindex, getindex, hash, in, isempty, isequal, lastindex, length, push!,
+  read, setindex!, show, size, sizeof, sort, write
+import Base.Libc: TmStruct
+import DSP: filtfilt
