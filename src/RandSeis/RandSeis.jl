9a7a7041ca522c8d3382a7b2b8ed4f0c1e6e7572
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 03:13:02 -0700

    update import/export

--- src/RandSeis/RandSeis.jl
+++ src/RandSeis/RandSeis.jl
@@ -1,9 +1,10 @@
 module RandSeis
-export randSeisChannel, randSeisData, randSeisEvent, randSeisHdr
-
-using ..SeisIO: SeisData, SeisChannel, SeisHdr, SeisEvent, code2typ, getbandcode, note!, μs, sμ
+using ..SeisIO: EQLoc, EventTraceData, GeoLoc, PZResp, PZResp64, SeisData, SeisChannel, SeisHdr, SeisEvent,
+  code2typ, getbandcode, note!, sμ, μs
 import Dates:now
 import Random:randexp, randstring, shuffle!
+
+export randSeisChannel, randSeisData, randSeisEvent, randSeisHdr
 
 include("randseis_utils.jl")
 include("randSeisChannel.jl")
