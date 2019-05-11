8744549d9cc3e4c30fb87be81b82cf2dd0b746e7
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 19:29:49 -0700

    trivial changes

--- src/Types/GphysChannel.jl
+++ src/Types/GphysChannel.jl
@@ -12,12 +12,12 @@ function isequal(C::T, D::T) where {T<:GphysChannel}
 end
 ==(C::T, D::T) where {T<:GphysChannel} = isequal(C,D)
 
-"""
+@doc """
     findid(C::SeisChannel, S::SeisData)
     findid(S::SeisData, C::SeisChannel)
 
 Get the index to the first channel `c` in S where `S.id[c]==C.id`.
-"""
+""" findid
 findid(C::TC, S::TS) where {TC<:GphysChannel, TS<:GphysData} = findid(getfield(C, :id), S)
 findid(S::TS, C::TC) where {TC<:GphysChannel, TS<:GphysData} = findid(getfield(C, :id), S)
 in(s::String, C::GphysChannel) = getfield(C, :id)==s
