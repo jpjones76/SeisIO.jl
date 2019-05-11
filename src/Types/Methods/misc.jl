9bbf9e78fa439cb1063b0cd35de64d42f745893e
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 19:30:05 -0700

    added EventTraceData(U...)

--- src/Types/Methods/misc.jl
+++ src/Types/Methods/misc.jl
@@ -14,3 +14,20 @@ function SeisData(U...)
   end
   return S
 end
+
+function EventTraceData(U...)
+  TD = EventTraceData()
+  for i = 1:length(U)
+    Y = getindex(U,i)
+    if typeof(Y) == SeisChannel
+      push!(S, convert(EventChannel, Y))
+    elseif typeof(Y) == SeisData
+      append!(S, convert(EventTraceData, Y))
+    elseif typeof(Y) == SeisEvent
+      append!(S, getfield(Y, :data))
+    else
+      @warn(string("Tried to join incompatible type into SeisData at arg ", i, "; skipped."))
+    end
+  end
+  return S
+end
