f65e07d2c375378e4c932dbaf732ff8f6ddb866e
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 21:12:17 -0700

    added randPhaseCat

--- src/RandSeis/randSeisEvent.jl
+++ src/RandSeis/randSeisEvent.jl
@@ -1,5 +1,3 @@
-export randSeisHdr, randSeisEvent
-
 """
     randSeisHdr()
 
@@ -28,10 +26,44 @@ function randSeisHdr()
 end
 
 """
+    randPhaseCat()
+
+Generate a random seismic phase catalog suitable for testing EventChannel,
+EventTraceData, and SeisEvent objects.
+"""
+function randPhaseCat()
+  phase_list = String["P", "PKIKKIKP", "PKIKKIKS", "PKIKPPKIKP", "PKPPKP",
+  "PKiKP", "PP", "PS", "PcP", "S", "SKIKKIKP", "SKIKKIKS", "SKIKSSKIKS", "SKKS",
+  "SKS", "SKiKP", "SP", "SS", "ScS", "pP", "pPKiKP", "pS", "pSKS", "sP",
+  "sPKiKP", "sS", "sSKS"]
+  pol_list = Char['U', 'D', '-', '+', '_', ' ']
+
+  phase = Array{String,1}(undef, rand(3:18))
+  for j = 1:length(phase)
+    phase[j] = rand(phase_list)
+  end
+  unique!(phase)
+
+  P = PhaseCat()
+  for j in phase
+    P[j] = SeisPha(rand(), rand(), rand(), rand(), rand(), rand(pol_list))
+  end
+  return P
+end
+
+"""
     randSeisEvent([, c=0.2, s=0.6])
 
 Generate a SeisEvent structure filled with random header and channel data.
 * 100*c is the percentage of :data channels _after the first_ with irregularly-sampled data (fs = 0.0)
 * 100*s is the percentage of :data channels _after the first_ with guaranteed seismic data.
 """
-randSeisEvent(; c=0.2::Float64, s=0.6::Float64) = SeisEvent(hdr=randSeisHdr(), data=convert(EventTraceData, randSeisData(c=c, s=s)))
+function randSeisEvent(; c=0.2::Float64, s=0.6::Float64)
+  V = SeisEvent(hdr=randSeisHdr(), data=convert(EventTraceData, randSeisData(c=c, s=s)))
+
+
+  for i = 1:V.data.n
+    setindex!(getfield(getfield(V, :data), :pha), randPhaseCat(), i)
+  end
+  return V
+end
