1bed045d945a101a8ae3ee11ac945f416aef04ba
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 19:28:35 -0700

    extend convert to SeisChannel, EventChannel

--- src/Types/Methods/convert.jl
+++ src/Types/Methods/convert.jl
@@ -3,8 +3,7 @@ convert(::Type{SeisData}, C::SeisChannel) = SeisData(C)
 
 function convert(::Type{EventTraceData}, S::SeisData)
   TD = EventTraceData(getfield(S, :n))
-  F = fieldnames(SeisData)
-  for f in F
+  for f in datafields
     if (f in unindexed_fields) == false
       setfield!(TD, f, deepcopy(getfield(S, f)))
     end
@@ -14,11 +13,26 @@ end
 
 function convert(::Type{SeisData}, TD::EventTraceData)
   S = SeisData(getfield(TD, :n))
-  F = fieldnames(SeisData)
-  for f in F
+  for f in datafields
     if (f in unindexed_fields) == false
       setfield!(S, f, deepcopy(getfield(TD, f)))
     end
   end
   return S
+end
+
+function convert(::Type{SeisChannel}, D::EventChannel)
+  C = SeisChannel()
+  for f in datafields
+    setfield!(C, f, deepcopy(getfield(D, f)))
+  end
+  return C
+end
+
+function convert(::Type{EventChannel}, C::SeisChannel)
+  D = EventChannel()
+  for f in datafields
+    setfield!(D, f, deepcopy(getfield(D, f)))
+  end
+  return D
 end
