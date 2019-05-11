c0dd6d83d82f02e5ab736f1891f7937ffd847ff6
Joshua Jones <jpjones76@users.noreply.github.com>
2019-05-10 21:15:25 -0700

    extended convert methods
    Former-commit-id: 5937e94d1f83a0750648008850d5d1174f47730d
    Former-commit-id: ddda1d64f406501acd990921f99cf8ccbff07283
    Former-commit-id: b891220024eee633d9b2bdc1822bdbab4009d94a

--- src/Types/Methods/misc.jl
+++ src/Types/Methods/misc.jl
@@ -4,8 +4,12 @@ function SeisData(U...)
     Y = getindex(U,i)
     if typeof(Y) == SeisChannel
       push!(S, Y)
+    elseif typeof(Y) == EventChannel
+        push!(S, convert(SeisChannel, Y))
     elseif typeof(Y) == SeisData
       append!(S, Y)
+    elseif typeof(Y) == EventTraceData
+      append!(S, convert(SeisData, Y))
     elseif typeof(Y) == SeisEvent
       append!(S, convert(SeisData, getfield(Y, :data)))
     else
@@ -20,14 +24,18 @@ function EventTraceData(U...)
   for i = 1:length(U)
     Y = getindex(U,i)
     if typeof(Y) == SeisChannel
-      push!(S, convert(EventChannel, Y))
+      push!(TD, convert(EventChannel, Y))
+    elseif typeof(Y) == EventChannel
+      push!(TD, Y)
     elseif typeof(Y) == SeisData
-      append!(S, convert(EventTraceData, Y))
+      append!(TD, convert(EventTraceData, Y))
     elseif typeof(Y) == SeisEvent
-      append!(S, getfield(Y, :data))
+      append!(TD, getfield(Y, :data))
+    elseif typeof(Y) == EventTraceData
+      append!(TD, Y)
     else
       @warn(string("Tried to join incompatible type into SeisData at arg ", i, "; skipped."))
     end
   end
-  return S
+  return TD
 end
