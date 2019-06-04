8b33a66df50cb5a39b043cd7d72191321d5c1c3d
Joshua Jones <jpjones76@users.noreply.github.com>
2019-06-04 01:54:23 -0700

    extended read, write to SeisData

--- src/Types/SeisData.jl
+++ src/Types/SeisData.jl
@@ -21,7 +21,7 @@ A container for earthquake source information; specific to seismology.
 A structure for discrete seismic events, comprising a SeisHdr for the event
   descriptor and a SeisData for data.
 
-## Fields: SeisData, SeisChannel, SeisEvent.data
+## Fields: SeisData, SeisChannel
 
 | **Field** | **Description** |
 |:-------|:------ |
@@ -78,6 +78,26 @@ mutable struct SeisData <: GphysData
                 Array{FloatArray,1}(undef,0),
                 Array{TCPSocket,1}(undef,0)
               )
+  end
+
+  function SeisData( n::Int64,
+            id::Array{String,1}                 , # id
+            name::Array{String,1}               , # name
+            loc::Array{InstrumentPosition,1}    , # loc
+            fs::Array{Float64,1}                , # fs
+            gain::Array{Float64,1}              , # gain
+            resp::Array{InstrumentResponse,1}   , # resp
+            units::Array{String,1}              , # units
+            src::Array{String,1}                , # src
+            misc::Array{Dict{String,Any},1}     , # misc
+            notes::Array{Array{String,1},1}     , # notes
+            t::Array{Array{Int64,2},1}          , # time
+            x::Array{FloatArray,1})
+
+    return new(n,
+      id, name, loc, fs, gain, resp, units, src, misc, notes, t, x,
+      Array{TCPSocket,1}(undef,0)
+      )
   end
 
   function SeisData(n::UInt)
@@ -146,4 +166,128 @@ function sizeof(S::SeisData)
   return s
 end
 
-(:n, :c, :name, :id, :units, :misc)
+function read(io::IO, ::Type{SeisData})
+  Z = getfield(BUF, :buf)
+  L = getfield(BUF, :int64_buf)
+
+  # read begins ------------------------------------------------------
+  N     = read(io, Int64)
+  checkbuf_strict!(L, 2*N)
+  readbytes!(io, Z, 3*N)
+  c1    = copy(Z[1:N])
+  c2    = copy(Z[N+1:2*N])
+  y     = code2typ.(getindex(Z, 2*N+1:3*N))
+  cmp   = read(io, Bool)
+  read!(io, L)
+  nx    = getindex(L, N+1:2*N)
+
+  if cmp
+    checkbuf_8!(Z, maximum(nx))
+  end
+
+  return SeisData(N,
+    read_string_vec(io, Z),
+    read_string_vec(io, Z),
+    InstrumentPosition[read(io, code2loctyp(getindex(c1, i))) for i = 1:N],
+    read!(io, Array{Float64, 1}(undef, N)),
+    read!(io, Array{Float64, 1}(undef, N)),
+    InstrumentResponse[read(io, code2resptyp(getindex(c2, i))) for i = 1:N],
+    read_string_vec(io, Z),
+    read_string_vec(io, Z),
+    [read_misc(io, Z) for i = 1:N],
+    [read_string_vec(io, Z) for i = 1:N],
+    [read!(io, Array{Int64, 2}(undef, getindex(L, i), 2)) for i = 1:N],
+    FloatArray[cmp ?
+      Blosc.decompress(getindex(y,i), readbytes!(io, Z, getindex(nx, i))) :
+      read!(io, Array{getindex(y,i), 1}(undef, getindex(nx, i)))
+      for i = 1:N])
+end
+
+function write(io::IO, S::SeisData)
+  N     = getfield(S, :n)
+  LOC   = getfield(S, :loc)
+  RESP  = getfield(S, :resp)
+  T     = getfield(S, :t)
+  X     = getfield(S, :x)
+  MISC  = getfield(S, :misc)
+  NOTES = getfield(S, :notes)
+
+  cmp = false
+  if KW.comp != 0x00
+    nx_max = maximum([sizeof(getindex(X, i)) for i = 1:S.n])
+    if (nx_max > KW.n_zip) || (KW.comp == 0x02)
+      cmp = true
+      Z = getfield(BUF, :buf)
+      checkbuf_8!(Z, nx_max)
+    end
+  end
+
+  codes = Array{UInt8,1}(undef, 3*N)
+  L = Array{Int64,1}(undef, 2*N)
+
+  # write begins ------------------------------------------------------
+  write(io, N)
+  p = position(io)
+  skip(io, 19*N+1)
+
+  write_string_vec(io, S.id)                                          # id
+  write_string_vec(io, S.name)                                        # name
+  i = 0                                                               # loc
+  while i < N
+    i = i + 1
+    loc = getindex(LOC, i)
+    setindex!(codes, loctyp2code(loc), i)
+    write(io, loc)
+  end
+  write(io, S.fs)                                                     # fs
+  write(io, S.gain)                                                   # gain
+  i = 0                                                               # resp
+  while i < N
+    i = i + 1
+    resp = getindex(RESP, i)
+    setindex!(codes, resptyp2code(resp), N+i)
+    write(io, resp)
+  end
+  write_string_vec(io, S.units)                                       # units
+  write_string_vec(io, S.src)                                         # src
+
+  for i = 1:N; write_misc(io, getindex(MISC, i)); end                 # misc
+  for i = 1:N; write_string_vec(io, getindex(NOTES, i)); end          # notes
+  i = 0                                                               # t
+  while i < N
+    i = i + 1
+    t = getindex(T, i)
+    setindex!(L, size(t,1), i)
+    write(io, t)
+  end
+  i = 0                                                               # x
+  while i < N
+    i = i + 1
+    x = getindex(X, i)
+    nx = lastindex(x)
+    if cmp
+      while l == zero(Int64)
+        l = Blosc.compress!(Z, x, level=5)
+        (l > zero(Int64)) && break
+        nx_max = nextpow(2, nx_max)
+        @warn(string("Compression ratio > 1.0 for channel ", i, "; are data OK?"))
+      end
+      xc = view(Z, 1:l)
+      write(io, xc)
+      setindex!(L, l, N+i)
+    else
+      write(io, x)
+      setindex!(L, nx, N+i)
+    end
+    setindex!(codes, typ2code(eltype(x)), 2*N+i)
+  end
+  q = position(io)
+
+  seek(io, p)
+  write(io, codes)
+  write(io, cmp)
+  write(io, L)
+  seek(io, q)
+  # write ends ------------------------------------------------------
+  return nothing
+end
