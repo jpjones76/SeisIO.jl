7ad041a7e757d96ebc538c1bf870927618ab6535
Joshua Jones <jpjones76@users.noreply.github.com>
2019-06-04 01:57:06 -0700

    minor functionality changes to RandSeis
    Former-commit-id: bd25bb93fd609d7ce8839ae9c12018bca4d3ef06
    Former-commit-id: b1b3de1aefd43f614cbce10ce928f833c95c3f29
    Former-commit-id: e5a9eff9cee30c1fdc94bcac0b16e83fc0c9e2d2

--- src/RandSeis/randSeisChannel.jl
+++ src/RandSeis/randSeisChannel.jl
@@ -1,152 +1,150 @@
 # Things that work for both regularly and irregularly sampled data
-function pop_chan_tail!(Ch::SeisChannel)
-  ((Ch.gain == 1) || isnan(Ch.gain)) && (Ch.gain = rand()*10^rand(0:10))    # gain
-  if isempty(Ch.loc)
-    loc = GeoLoc()
-    for f in fieldnames(GeoLoc)
-      x = rand(Float64)
-      if f != :datum
-        if f == :lat || f == :inc
-          x = 180*(x-0.5)
-        elseif f == :lon || f == :az
-          x = 360*(x-0.5)
-        end
-        setfield!(loc, f, x)
-      else
-        setfield!(loc, f, x > 0.5 ? "WGS-84" : x > 0.25 ? "ETRS89" : x > 0.1 ? "GRS 80" : "JGD2011")
-      end
-    end
-  end                                                                       # loc
-  if isempty(Ch.misc)
-    pop_rand_dict!(Ch.misc, rand(4:24))                                     # misc
-  end
-  note!(Ch, "Created by function populate_chan!.")                          # note
-end
-
-# Populate a channel with irregularly-sampled (campaign-style) data
-function populate_irr!(Ch::SeisChannel; nx::Int64=0)
-  irregular_units = ["%", "(% cloud cover)", "(direction vector)", "C", "K", "None", "Pa", "T", "V", "W", "m", "m/m", "m/s", "m/s^2", "m^3/m^3", "rad", "rad/s", "rad/s^2", "tonnes SO2"]
-
-  Ch.fs = 0
-  if isempty(Ch.id) || Ch.id == "...YYY"
-    chan = "OY"*randstring('A':'Z',1)
-    net = ur2()
-    sta = uppercase(randstring('A':'Z', rand(1:5)))
-    loc = ur2()
-
-    # id
-    Ch.id = join([net,sta,loc,chan],'.')
-
-  end
-
-  # units
-  if isempty(Ch.units) || units == lowercase("unknown")
-    Ch.units = rand(irregular_units)
-  end
-
-  if isempty(Ch.x) || isempty(Ch.t)
-    ts = round(Int, sμ*(time()-86400+randn()))
-    if nx == 0
-      L = 2^rand(6:12)
-    else
-      L = nx
-    end
-    Ls = rand(1200:7200)
-    Ch.x = (rand(L) .- (rand(Bool) == true ? 0.5 :  0.0)).*(10 .^ (rand(1:10, L)))
-    Ch.t = hcat(collect(1:1:L), ts.+sort(rand(UnitRange{Int64}(1:Ls), L)))
-  end
-  Ch.src = string("randSeisChannel(c=true, nx=",  nx, ")")
-
-  pop_chan_tail!(Ch)
-  return nothing
-end
-
-# Populate a channel with regularly-sampled (time-series) data
-function populate_chan!(Ch::SeisChannel; s::Bool=false, nx::Int64=0)
-  fc_vals = Float64[1/120 1/60 1/30 0.2 1.0 1.0 1.0 2.0 4.5 15.0]
-  fs_vals = Float64[0.1, 1.0, 2.0, 5.0, 10.0, 20.0, 25.0, 40.0, 50.0, 60.0, 62.5,
-    80.0, 100.0, 120.0, 125.0, 250.0]
-  bcodes = Char['V', 'L', 'M', 'M', 'B', 'S', 'S', 'S', 'S', 'S', 'S', 'H', 'S', 'E', 'E', 'C']
-
-  # Ch.name
-  isempty(Ch.name) && (Ch.name = randstring(12))
-
-  # Ch.fs
-  (isempty(Ch.fs) || Ch.fs == 0 || isnan(Ch.fs)) && (Ch.fs = rand(fs_vals))
-
-  fc = rand(fc_vals[fc_vals .< Ch.fs/2])
-
-  # An empty ID generates codes and units to match values real data might have
-  if isempty(Ch.id) || Ch.id == "...YYY"
-    bcode = getbandcode(Ch.fs)
-    (icode,ccode,units) = getyp2codes(bcode, s)
-    chan = join([bcode, icode, ccode])
-    net = ur2()
-    sta = uppercase(randstring('A':'Z', rand(1:5)))
-    loc = rand() < 0.3 ? "" : ur2()
-    Ch.id = join([net,sta,"",chan],'.')                                     # id
-    if isempty(Ch.units)
-      Ch.units = units                                                      # units
-    end
-  end
-
-  # Need this even if Ch had an ID value when populate_chan! was called
-  cha = split(Ch.id, '.')[4]
-  ccode = cha[2]
-
-  # A random instrument response function
-  if isempty(Ch.resp)
+function randResp(n::Int64=0)
+  if n > 0
+    T = Float64
+    i = n
+  else
     T = rand() < 0.5 ? Float32 : Float64
-    i = rand(1:4)
-    zstub = zeros(T, 2*i)
-    pstub = 10 .*rand(T, i)
-    if T == Float32
-      Ch.resp = PZResp(0.0f0, complex.(zstub), vcat(pstub .+ pstub.*im, pstub .- pstub*im))    # resp
-    else
-      Ch.resp = PZResp64(0.0, complex.(zstub), vcat(pstub .+ pstub.*im, pstub .- pstub*im))    # resp
-    end
+    i = rand(2:2:8)
   end
+  zstub = zeros(T, 2*i)
+  pstub = 10 .*rand(T, i)
+  if T == Float32
+    resp = PZResp(0.0f0, complex.(zstub), vcat(pstub .+ pstub.*im, pstub .- pstub*im))    # resp
+  else
+    resp = PZResp64(0.0, complex.(zstub), vcat(pstub .+ pstub.*im, pstub .- pstub*im))    # resp
+  end
+  return resp
+end
 
-  # random noise for data, with random short time gaps; gaussian noise for a
-  # time series, uniform noise with a random exponent otherwise
-  if isempty(Ch.x) || isempty(Ch.t)                                         # x
+function rand_t(Lx::Int64, n::Int64=0)
+  ts = time()-86400+randn()
+  L = n == 0 ? rand(0:9) : n
+  t = zeros(2+L, 2)
 
-    if nx == 0
-      # Change: length is always 20-120 minutes
-      Ls = rand(1200:7200)
-      Lx = ceil(Int, Ls*Ch.fs)
-    else
-      Lx = nx
-    end
-    Ch.x = randn(rand() < 0.5 ? Float32 : Float64, Lx)
+  # first row is always start time
+  t[1,:] = [1 round(Int64, ts/μs)]
 
-    ts = time()-86400+randn()                                               # t
-    L = rand(0:9)
-    t = zeros(2+L, 2)
-
-    # first row is always start time
-    t[1,:] = [1 round(Int64, ts/μs)]
-
-    # rest are random time gaps
+  # rest are random time gaps
+  if L > 0
     gaps = unique(rand(2:Lx, L, 1))
-    while length(unique(gaps)) < L
-      gaps = unique(rand(2:Lx, L, 1))
+
+    # try to create exactly L gaps
+    j = 0
+    while j < 5
+      j = j + 1
+      gaps_tmp = unique(rand(2:Lx, L, 1))
+      if length(gaps_tmp) > length(gaps)
+        gaps = gaps_tmp
+      end
+      (length(gaps) == L) && break
     end
+    L = length(gaps)
+    
     t[2:L+1,1] = gaps
     for i = 2:L+1
       t[i,2] = round(Int64, (rand(1:100)+rand())*sμ)
     end
 
     # control for gap in last sample
-    if any(t[:,1].==length(Ch.x)) == true
+    if any(t[:,1].==Lx) == true
       t = t[1:L+1,:]
     else
       t[L+2,:] = [Lx 0]
     end
-    Ch.t = sortslices(t, dims=1)
+  else
+    t[2,:] = [Lx 0]
   end
+  return sortslices(t, dims=1)
+end
 
-  Ch.src = string("randSeisChannel(c=false, nx=",  nx, ")")
+function randLoc(randtype::Bool=true)
+  x = rand()
+  y = randtype == true ? rand() : 1.0
+  datum = x > 0.5 ? "WGS-84 " : x > 0.25 ? "ETRS89 " : x > 0.1 ? "GRS 80 " : "JGD2011"
+  loc = [ 180*(rand()-0.5),
+          360*(rand()-0.5),
+          1000*rand(),
+          100*rand(),
+          360*(rand()-0.5),
+          180*(rand()-0.5) ]
+  if y > 0.5
+    return GeoLoc(datum, loc...)
+  else
+    g = GenLoc(loc)
+    g.datum = datum
+    return g
+  end
+end
+
+function pop_chan_tail!(Ch::SeisChannel)
+  setfield!(Ch, :name, randstring(rand(12:64)))       # name
+  setfield!(Ch, :gain,  rand()*10^rand(0:10))         # gain
+  setfield!(Ch, :loc, randLoc())                      # loc
+  pop_rand_dict!(Ch.misc, rand(4:24))                 # misc
+  note!(Ch, "Created by function populate_chan!.")    # notes
+end
+
+# Populate a channel with irregularly-sampled (campaign-style) data
+function populate_irr!(Ch::SeisChannel; nx::Int64=0)
+  irregular_units = ["%", "(% cloud cover)", "(direction vector)", "C", "K", "None", "Pa", "T", "V", "W", "m", "m/m", "m/s", "m/s^2", "m^3/m^3", "rad", "rad/s", "rad/s^2", "tonnes SO2"]
+
+  chan = "OY"*randstring('A':'Z',1)
+  net = ur2()
+  sta = uppercase(randstring('A':'Z', rand(1:5)))
+  loc = ur2()
+
+  ts = round(Int, sμ*(time()-86400+randn()))
+  if nx == 0
+    L = 2^rand(6:12)
+  else
+    L = nx
+  end
+  Ls = rand(1200:7200)
+
+  Ch.id     = join([net,sta,loc,chan],'.')
+  Ch.fs     = 0
+  Ch.src    = string("randSeisChannel(c=true, nx=",  nx, ")")
+  Ch.units  = rand(irregular_units)
+  Ch.t      = hcat(collect(1:1:L), ts.+sort(rand(UnitRange{Int64}(1:Ls), L)))
+  Ch.x      = (rand(L) .- (rand(Bool) == true ? 0.5 :  0.0)).*(10 .^ (rand(1:10, L)))
+  pop_chan_tail!(Ch)
+  return nothing
+end
+
+# Populate a channel with regularly-sampled (time-series) data
+function populate_chan!(Ch::SeisChannel; s::Bool=false, nx::Int64=0)
+  fc_vals = Float64[1.0/120.0 1.0/60.0 1.0/30.0 0.2 1.0 1.0 1.0 2.0 4.5 15.0]
+  fs_vals = Float64[0.1, 1.0, 2.0, 5.0, 10.0, 20.0, 25.0, 40.0, 50.0, 60.0, 62.5, 80.0, 100.0, 120.0, 125.0, 250.0]
+  bcodes  = Char['V', 'L', 'M', 'M', 'B', 'S', 'S', 'S', 'S', 'S', 'S', 'H', 'S', 'E', 'E', 'C']
+
+  # Ch.fs
+  fs = rand(fs_vals)
+  fc = rand(fc_vals[fc_vals .< fs/2])
+
+  # An empty ID generates codes and units to match values real data might have
+  bc = getbandcode(fs)
+  (ic, cc, units) = getyp2codes(bc, s)
+  sta = randstring('A':'Z', rand(3:5))
+  loc = rand() < 0.5 ? "" : ur2()
+  cha = string(bc, ic, cc)
+
+  Ch.id     = join([ur2(), sta, loc, cha],'.')                        # id
+  Ch.fs     = fs                                                      # fs
+  Ch.units  = units                                                   # units
+  Ch.resp   = randResp()                                              # resp
+  Ch.src    = string("randSeisChannel(c=false, nx=",  nx, ")")        # src
+
+  # random noise for data, with random time gaps
+  if nx == 0
+    Ls = rand(1200:7200)
+    Lx = ceil(Int, Ls*Ch.fs)
+  else
+    Lx = nx
+  end
+  Ch.x = randn(rand() < 0.5 ? Float32 : Float64, Lx)                  # x
+  Ch.t = rand_t(Lx)                                                   # t
+
   pop_chan_tail!(Ch)
   return nothing
 end
--- src/RandSeis/randSeisEvent.jl
+++ src/RandSeis/randSeisEvent.jl
@@ -1,3 +1,85 @@
+const evtypes = String[ "not_existing",
+                        "not_reported",
+                        "anthropogenic_event",
+                        "collapse",
+                        "cavity_collapse",
+                        "mine_collapse",
+                        "building_collapse",
+                        "explosion",
+                        "accidental_explosion",
+                        "chemical_explosion",
+                        "controlled_explosion",
+                        "experimental_explosion",
+                        "industrial_explosion",
+                        "mining_explosion",
+                        "quarry_blast",
+                        "road_cut",
+                        "blasting_levee",
+                        "nuclear_explosion",
+                        "induced_or_triggered_event",
+                        "rock_burst",
+                        "reservoir_loading",
+                        "fluid_injection",
+                        "fluid_extraction",
+                        "crash",
+                        "plane_crash",
+                        "train_crash",
+                        "boat_crash",
+                        "other_event",
+                        "atmospheric_event",
+                        "sonic_boom",
+                        "sonic_blast",
+                        "acoustic_noise",
+                        "thunder",
+                        "avalanche",
+                        "snow_avalanche",
+                        "debris_avalanche",
+                        "hydroacoustic_event",
+                        "ice_quake",
+                        "slide",
+                        "landslide",
+                        "rockslide",
+                        "meteorite",
+                        "volcanic_eruption"]
+
+
+"""
+    randSeisSrc()
+
+Generate a SeisSrc structure filled with random values.
+"""
+function randSeisSrc(; mw::Float32=0.0f0)
+  R = SeisSrc()
+
+#    src:
+#     st: dur 0.0, rise 0.0, decay 0.0
+
+  m0 = 1.0e-7*(10^(1.5*((mw == zero(Float32) ? 10^(rand(Float64)-1.0) : Float64(mw))+10.7)))
+
+  setfield!(R, :id, string(rand(1:2^62)))                           # :id
+  setfield!(R, :m0, m0)                                             # :m0
+  setfield!(R, :gap, rand(Float64)*180.0)                           # :gap
+  pop_rand_dict!(getfield(R, :misc), rand(4:24))                    # :misc
+  setfield!(R, :mt, m0.*(rand(Float64, 6).-0.5))                    # :mt
+  setfield!(R, :dm, m0.*(rand(Float64, 6).-0.5))                    # :dm
+  setfield!(R, :npol, rand(Int64(6):Int64(120)))                    # :npol
+  [note!(R, randstring(rand(16:256))) for i = 1:rand(1:6)]          # :notes
+  setfield!(R, :pax,
+    vcat(360.0.*(rand(Float64, 2, 2).-0.5),
+          (rand(-1:2:1)*m0).*[rand() -1.0*rand()]))                 # :pax
+  setfield!(R, :planes,
+    360.0.*(rand(Float64, rand(2:3), rand(2:3)).-0.5))              # :planes
+  setfield!(R, :src, "randSeisSrc")                                 # :src
+  # setfield!(R, :src, join([join([randstring(12), " ",
+  #                           randstring(20)]) for i=1:5], ","
+  #                         ))                                        # :src
+  setfield!(R, :st, SourceTime( randstring(2^rand(6:8)),            # :st
+                                rand(Float64),
+                                rand(Float64),
+                                rand(Float64)))
+  return R
+end
+
 """
     randSeisHdr()
 
@@ -5,23 +87,55 @@ Generate a SeisHdr structure filled with random values.
 """
 function randSeisHdr()
   H = SeisHdr()
+
+  # a random earthquake location
   loc = EQLoc()
-  loc.lat = (rand(0.0:1.0:89.0)+rand())*-1.0^(rand(1:2))
-  loc.lon = (rand(0.0:1.0:179.0)+rand())*-1.0^(rand(1:2))
-  loc.dep = min(10.0*randexp(Float64), 660.0)
+  setfield!(loc, :lat, (rand(0.0:1.0:89.0)+rand())*-1.0^(rand(1:2)))
+  setfield!(loc, :lon, (rand(0.0:1.0:179.0)+rand())*-1.0^(rand(1:2)))
+  setfield!(loc, :dep, (min(10.0*randexp(Float64), 660.0)))
+  setfield!(loc, :dx, rand()*4.0)
+  setfield!(loc, :dy, rand()*4.0)
+  setfield!(loc, :dz, rand()*8.0)
+  setfield!(loc, :src, rand(["HYPOELLIPSE", "HypoDD", "Velest", "centroid"]))
+  setfield!(loc, :typ, "hypocenter")
+  setfield!(loc, :sig, "1σ")
 
-  setfield!(H, :id, rand(1:2^62))
-  setfield!(H, :ot, now())
-  setfield!(H, :loc, loc)
-  setfield!(H, :mag, (6.0f0*rand(Float32), "M_"*randstring(rand(2:4))))
-  setfield!(H, :int, (UInt8(floor(Int, H.mag[1])), randstring(rand(2:4))))
-  setfield!(H, :mt, rand(Float64, 8))
-  setfield!(H, :axes, [(rand(), rand(), rand()), (rand(), rand(), rand()), (rand(), rand(), rand())])
-  setfield!(H, :src, "randSeisHdr")
-  pop_rand_dict!(H.misc, rand(4:24))
-  [note!(H, randstring(rand(16:256))) for i = 1:rand(3:18)]
+  # moment magnitude and m0 in SI units
+  mw = 10^(rand(Float32)-1.0f0)
+  m0 = 1.0e-7*(10^(1.5*(Float64(mw)+10.7)))
 
-  # Adding a phase catalog
+  # event type
+  rtyp = rand()
+  typ = rtyp > 0.3 ? "earthquake" : rand(evtypes)
+
+  # Generate random header
+  setfield!(H, :id, string(rand(1:2^62)))                           # :id
+  setfield!(H, :int, (floor(UInt8, mw), randstring(rand(2:4))))     # :int
+  setfield!(H, :loc, loc)                                           # :loc
+  setfield!(H, :mag, EQMag( val   = mw,                             # :mag
+                            scale = rand(["M","m"]) * randstring(2),
+                            nst   = rand(3:100),
+                            gap   = 360.0*rand(Float64),
+                            src   = randstring(24)
+                            )
+            )
+  # setfield!(H, :mech, FocalMech(src = join([join([randstring(12),     # :mech
+  #                                     " ",
+  #                                     randstring(20)]) for i=1:5], ","),
+  #                               np  = 360.0.*(rand(Float64, 3, 2).-0.5),
+  #                               pax = vcat(360.0.*(rand(Float64, 2, 2).-0.5),
+  #                                     (rand(-1:2:1)*m0).*[rand() -1.0*rand()]),
+  #                               m0  = m0,
+  #                               mt  = m0.*(rand(Float64, 6).-0.5),
+  #                               dm  = m0.*(rand(Float64, 6).-0.5)
+  #                               )
+  #         ) # obviously not pure DC, jajaja "random"
+  pop_rand_dict!(H.misc, rand(4:24))                                # :misc
+  [note!(H, randstring(rand(16:256))) for i = 1:rand(1:6)]          # :notes
+  setfield!(H, :ot, now())                                          # :ot
+  setfield!(H, :src, "randSeisHdr")                                 # :src
+  setfield!(H, :typ, typ)                                           # :typ
+
   return H
 end
 
@@ -31,14 +145,14 @@ end
 Generate a random seismic phase catalog suitable for testing EventChannel,
 EventTraceData, and SeisEvent objects.
 """
-function randPhaseCat()
+function randPhaseCat(n::Int64=0)
   phase_list = String["P", "PKIKKIKP", "PKIKKIKS", "PKIKPPKIKP", "PKPPKP",
   "PKiKP", "PP", "PS", "PcP", "S", "SKIKKIKP", "SKIKKIKS", "SKIKSSKIKS", "SKKS",
   "SKS", "SKiKP", "SP", "SS", "ScS", "pP", "pPKiKP", "pS", "pSKS", "sP",
   "sPKiKP", "sS", "sSKS"]
   pol_list = Char['U', 'D', '-', '+', '_', ' ']
 
-  phase = Array{String,1}(undef, rand(3:18))
+  phase = Array{String,1}(undef, n == 0 ? rand(3:18) : n)
   for j = 1:length(phase)
     phase[j] = rand(phase_list)
   end
@@ -46,7 +160,7 @@ function randPhaseCat()
 
   P = PhaseCat()
   for j in phase
-    P[j] = SeisPha(rand(), rand(), rand(), rand(), rand(), rand(pol_list))
+    P[j] = SeisPha(rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(pol_list), Char(rand(0x30:0x39)))
   end
   return P
 end
@@ -58,8 +172,10 @@ Generate a SeisEvent structure filled with random header and channel data.
 * 100*c is the percentage of :data channels _after the first_ with irregularly-sampled data (fs = 0.0)
 * 100*s is the percentage of :data channels _after the first_ with guaranteed seismic data.
 """
-function randSeisEvent(; c=0.2::Float64, s=0.6::Float64)
-  V = SeisEvent(hdr=randSeisHdr(), data=convert(EventTraceData, randSeisData(c=c, s=s)))
+function randSeisEvent(N::Int64; c::Float64=0.0, s::Float64=1.0, nx::Int64=0)
+  V = SeisEvent(hdr=randSeisHdr(),
+                source=randSeisSrc(),
+                data=convert(EventTraceData, randSeisData(N, c=c, s=s, nx=nx)))
 
   for i = 1:V.data.n
     setindex!(getfield(getfield(V, :data), :pha), randPhaseCat(), i)
@@ -69,3 +185,4 @@ function randSeisEvent(; c=0.2::Float64, s=0.6::Float64)
   end
   return V
 end
+randSeisEvent(; c::Float64=0.0, s::Float64=1.0, nx::Int64=0) = randSeisEvent(rand(8:24), c=c, s=s, nx=nx)
