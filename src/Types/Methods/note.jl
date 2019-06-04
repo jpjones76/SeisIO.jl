70fa0c150959eee864905e6eb68d17180861f2e2
Joshua Jones <jpjones76@users.noreply.github.com>
2019-06-04 01:50:55 -0700

    fixed method declarations
    Former-commit-id: 706ae9f61cdca2286191fcf326f58fb648ca6558
    Former-commit-id: 29cf666d07964cbdc4e467e724633bea7902cc12
    Former-commit-id: 47561dd8fb05ea2a7985704291e73cc288471081

--- src/Types/Methods/note.jl
+++ src/Types/Methods/note.jl
@@ -23,7 +23,7 @@ note!(S::T, i::Int64, s::String) where {T<:GphysData} = push!(S.notes[i], tnote(
 
 Append `s` to `S.notes` and time stamp. If `txt` contains a channel name or ID, only the channel mentioned is annotated; otherwise, all channels are annotated.
 """
-function note!(S::T, s::String) where {T<:GphysData}
+function note!(S::GphysData, s::String)
     J = [occursin(i, s) for i in S.name]
     K = [occursin(i, s) for i in S.id]
     j = findall(max.(J,K) .== true)
@@ -37,22 +37,23 @@ function note!(S::T, s::String) where {T<:GphysData}
   return nothing
 end
 
-function note!(S::T, id::String, s::String) where {T<:GphysData}
+function note!(S::GphysData, id::String, s::String)
   i = findid(id, S)
   (i == 0) && error(string("id = ", id, " not found in S!"))
   push!(S.notes[i], tnote(s))
   return nothing
 end
 
-function note!(S::T, chans::Array{Int64,1}, s::String) where {T<:GphysData}
+function note!(S::GphysData, chans::Array{Int64,1}, s::String)
   for c in chans
     note!(S, c, s)
   end
   return nothing
 end
 
-note!(H::SeisHdr, s::String) = push!(H.notes, tnote(s))
-note!(S::SeisChannel, s::String) = push!(S.notes, tnote(s))
+note!(S::GphysChannel, s::String) = push!(S.notes, tnote(s))
+# note!(R::SeisSrc, s::String) = push!(R.notes, tnote(s))
+# note!(H::SeisHdr, s::String) = push!(H.notes, tnote(s))
 
 # DND, these methods prevent memory reuse
 """
@@ -68,7 +69,7 @@ Clear all notes from channel `i` of `S` and leaves a note about this.
 
 As above for the first channel in `S` whose id is an exact match to `id`.
 """
-function clear_notes!(S::T) where {T<:GphysData}
+function clear_notes!(S::GphysData)
   cstr = tnote("notes cleared.")
   for i = 1:S.n
     empty!(S.notes[i])
@@ -77,13 +78,13 @@ function clear_notes!(S::T) where {T<:GphysData}
   return nothing
 end
 
-function clear_notes!(S::T, i::Int64) where {T<:GphysData}
+function clear_notes!(S::GphysData, i::Int64)
   empty!(S.notes[i])
   push!(S.notes[i], tnote("notes cleared."))
   return nothing
 end
 
-function clear_notes!(S::T, id::String) where {T<:GphysData}
+function clear_notes!(S::GphysData, id::String)
   i = findid(id, S)
   (i == 0) && error(string("id = ", id, " not found in S!"))
   empty!(S.notes[i])
@@ -91,5 +92,4 @@ function clear_notes!(S::T, id::String) where {T<:GphysData}
   return nothing
 end
 
-clear_notes!(U::Union{SeisChannel,SeisHdr}) = (U.notes = Array{String,1}(undef,1); U.notes[1] = tnote("notes cleared."); return nothing)
-clear_notes!(Ev::SeisEvent) = (clear_notes!(Ev.hdr); clear_notes!(Ev.data))
+clear_notes!(U::SeisChannel) = (U.notes = Array{String,1}(undef,1); U.notes[1] = tnote("notes cleared."); return nothing)
