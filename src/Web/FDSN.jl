2959e13c8f44182dd4926492fdddfb83dc86995e
Joshua Jones <jpjones76@users.noreply.github.com>
2019-06-04 01:49:28 -0700

    moved event-related functions to Quake submodule
    Former-commit-id: dbd7ced88022cdf606d5ce68f3fb5fc18780cd7c
    Former-commit-id: 40bef77e5a7f4580bec7abe8f30650158f0f5ff2
    Former-commit-id: bd117d88aaafdce61424d4cd59d4543bfe23f0cb

--- src/Web/FDSN.jl
+++ src/Web/FDSN.jl
@@ -1,9 +1,10 @@
-export FDSNevq, FDSNevt, FDSNsta
+# export FDSNevq, FDSNevt, FDSNsta
+export FDSNsta
 
 # =============================================================================
 # No export
 
-function FDSN_chp(chans::Union{String,Array{String,1},Array{String,2}}; v::Int64 = KW.v)
+function fdsn_chp(chans::Union{String,Array{String,1},Array{String,2}}; v::Int64 = KW.v)
   # Parse channel config
   if isa(chans, String)
     C = parse_chstr(chans, fdsn = true)
@@ -68,7 +69,7 @@ function FDSNsta(chans="*"::Union{String,Array{String,1},Array{String,2}};
     (isempty(reg) && isempty(rad)) && error("No query! Please specify a search radius, a rectangular search region, or some channels.")
     BODY *= string("* * * * ", d0, " ", d1, "\n")
   else
-    C = FDSN_chp(chans, v=v)
+    C = fdsn_chp(chans, v=v)
     Nc = size(C,1)
     for i = 1:Nc
       str = ""
@@ -85,7 +86,11 @@ function FDSNsta(chans="*"::Union{String,Array{String,1},Array{String,2}};
     println(BODY)
   end
   open(xf, "w") do io
-    request("POST", URL, webhdr, BODY, response_stream=io)
+    if occursin("ncedc", URL)
+      request("POST", URL, webhdr, BODY, response_stream=io, headers=["Host" => "service.ncedc.org", "User-Agent" => "curl/7.60.0", "Accept" => "*/*"])
+    else
+      request("POST", URL, webhdr, BODY, response_stream=io)
+    end
   end
 
   # Build channel list
@@ -131,9 +136,6 @@ function FDSNget!(U::SeisData, chans::Union{String,Array{String,1},Array{String,
   n_badreq = 0
   wc = "*"
   d0, d1 = parsetimewin(s, t)
-  # dt_end = DateTime(d1)
-  # dt1 = deepcopy(dt_end)
-  # dt0 = DateTime(d0)
 
   # (1) Time-space query for station info
   if si
@@ -166,11 +168,15 @@ function FDSNget!(U::SeisData, chans::Union{String,Array{String,1},Array{String,
   # (3) Data query
   v > 0 && @info(tnote("Data query begins"))
   URL = string(fdsn_uhead(src), "dataselect/1/query")
-  BODY = "format=" * fmt * "\n"
-  if !isempty(opts)
-    OPTS = split(opts, "&")
-    for opt in OPTS
-      BODY *= string(opt, "\n")
+  if occursin("ncedc", URL) == true
+    BODY = ""
+  else
+    BODY = "format=" * fmt * "\n"
+    if !isempty(opts)
+      OPTS = split(opts, "&")
+      for opt in OPTS
+        BODY *= string(opt, "\n")
+      end
     end
   end
 
@@ -220,7 +226,11 @@ function FDSNget!(U::SeisData, chans::Union{String,Array{String,1},Array{String,
                     '.')
 
       open(fname, "w") do io
-        request("POST", URL, webhdr, QUERY, readtimeout=to, response_stream=io)
+        if occursin("ncedc", URL)
+          request("POST", URL, webhdr, QUERY, readtimeout=to, response_stream=io, headers=["Host" => "service.ncedc.org", "User-Agent" => "curl/7.60.0", "Accept" => "*/*"])
+        else
+          request("POST", URL, webhdr, QUERY, readtimeout=to, response_stream=io)
+        end
       end
       io = open(fname, "r")
       parsable = true
@@ -256,168 +266,4 @@ function FDSNget!(U::SeisData, chans::Union{String,Array{String,1},Array{String,
   # Done!
   v > 0 && @info(tnote("Done FDSNget query."))
   return parse_err
-end
-
-"""
-    H = FDSNevq(ot)
-
-Multi-server query for the events with the closest origin time to `ot`.
-
-Standard keywords: evw, rad, reg, mag, nev, src, to
-
-See also: SeisIO.KW
-"""
-function FDSNevq(ot::String;
-  evw::Union{Array{Real,1},Array{Float64,1},Array{Int64,1}} = KW.evw,
-  rad::Array{Float64,1} = KW.rad,
-  reg::Array{Float64,1} = KW.reg,
-  mag::Array{Float64,1} = KW.mag,
-  nev::Int64 = KW.nev,
-  src::String = KW.src,
-  to::Int = KW.to,
-  v::Int64 = KW.v)
-
-  if isempty(reg) && !isempty(rad)
-    if length(rad) == 4
-      append!(rad, [-30.0, 700.0])
-    end
-    search_coords = string( "&latitude=", rad[1], "&longitude=", rad[2],
-                            "&minradius=", rad[3], "&maxradius=", rad[4],
-                            "&mindepth=", rad[5], "&maxdepth=", rad[6] )
-  else
-    if isempty(reg)
-      reg = Float64[-90.0, 90.0, -180.0, 180.0, -30.0, 700.0]
-    elseif length(reg) == 4
-      append!(reg, [-30.0, 700.0])
-    end
-    search_coords = string("&minlat=", reg[1], "&maxlat=", reg[2],
-                           "&minlon=", reg[3], "&maxlon=", reg[4],
-                           "&mindepth=", reg[5], "&maxdepth=", reg[6])
-  end
-
-  # Determine time window
-  ot2::Float64 = try
-    d2u(DateTime(ot))
-  catch
-    if length(ot) <= 14
-      ot0 = string(ot[1:4],"-",ot[5:6],"-",ot[7:8],"T",ot[9:10],":",ot[11:12])
-      if length(ot) > 12
-        ot1 = string(ot0, ":", ot[13:14])
-      else
-        ot1 = string(ot0, ":00")
-      end
-    end
-    d2u(DateTime(ot1))
-  end
-  d0 = string(u2d(ot2 - abs(evw[1])))
-  d1 = string(u2d(ot2 + abs(evw[2])))
-  oti = round(Int64, ot2*sμ)
-
-  # multi-server query (most FDSN servers do NOT have an event service)
-  if lowercase(src) == "all"
-    sources = String["EMSC", "INGV", "IRIS", "LMU", "NCEDC", "NIEP", "ORFEUS", "SCEDC", "USGS", "USP"]
-  else
-    sources = split(src,",")
-  end
-  sources = [strip(i) for i in sources]
-  catalog = Array{SeisHdr,1}(undef, 0)
-  origin_times = Array{Int64,1}(undef, 0)
-  for k in sources
-    v > 1 && println(stdout, "Querying ", k)
-    url = string(fdsn_uhead(String(k)), "event/1/query?",
-                                        "starttime=", d0, "&endtime=", d1,
-                                        search_coords,
-                                        "&minmag=", mag[1], "&maxmag=", mag[2],
-                                        "&format=xml")
-    v > 0 && println(stdout, "URL = ", url)
-    req_info_str = "\nFDSN event query:"
-
-    # R = request("GET", url, webhdr(), readtimeout=to)
-    (R, parsable) = get_HTTP_req(url, req_info_str, to)
-    if parsable
-      str_req = String(R)
-      v > 1 && println(stdout, "REQUEST BODY:\n", str_req)
-      (id, ot_tmp, loc, mm, msc) = FDSN_event_xml(str_req)
-      for i = 1:length(id)
-          eh = SeisHdr(id=id[i], ot=ot_tmp[i], loc=loc[i], mag=(mm[i], msc[i]), src=url)
-          push!(catalog, eh)
-          push!(origin_times, round(Int64, d2u(eh.ot)*sμ))
-      end
-      v > 1 && println(stdout, "CATALOG:\n", catalog)
-    end
-  end
-  k = sortperm(abs.(origin_times.-oti))
-  n0 = min(length(k), nev)
-  n0 < nev && @warn(string("Catalog only contains ", n0, " events (original request was ", nev,")"))
-  return catalog[k[1:n0]]
-end
-
-"""
-    FDSNevt(ot::String, chans::String)
-
-Get trace data for the event closest to origin time `ot` on channels `chans`.
-
-Standard keywords: fmt, mag, nd, opts, pha, rad, reg, src, to, v, w
-
-Other keywords:
-* len::Real (120.0): desired record length in minutes
-* model::String ("iasp91"): Earth velocity model for phase calculations
-
-See also: distaz!, FDSNevq, FDSNsta, SeisKW
-"""
-function FDSNevt(ot::String, chans::Union{String,Array{String,1},Array{String,2}};
-  len::Real             = 120.0,
-  evw::Union{Array{Real,1},Array{Float64,1},Array{Int64,1}} = KW.evw,
-  fmt::String           = KW.fmt,
-  mag::Array{Float64,1} = KW.mag,
-  model::String         = "iasp91",
-  nd::Real              = KW.nd,
-  opts::String          = KW.opts,
-  pha::String           = KW.pha,
-  rad::Array{Float64,1} = KW.rad,
-  reg::Array{Float64,1} = KW.reg,
-  src::String           = KW.src,
-  to::Int64             = KW.to,
-  v::Int64              = KW.v,
-  w::Bool               = KW.w
-  )
-
-  C = FDSN_chp(chans, v=v)
-
-  # Create header
-  v > 0 && println(stdout, now(), ": event query begins.")
-  H = FDSNevq(ot, nev=1,
-                  rad=rad,
-                  reg=reg,
-                  mag=mag,
-                  src=src,
-                  to=to,
-                  evw=evw,
-                  v=v
-              )[1]
-
-  # Create channel data
-  v > 0 && println(stdout, now(), ": data query begins.")
-  s = H.ot                                      # Start time for FDSNsta is event origin time
-  t = u2d(d2u(s) + 60*len)                      # End time is len minutes later
-  (d0, d1) = parsetimewin(s,t)
-  S = SeisData()
-  FDSNget!(S, C, fmt=fmt, nd=nd, rad=rad, reg=reg, s=d0, si=true, src=src, t=d1, to=to, v=v, w=w)
-
-  v > 1 && println(stdout, now(), ": channels initialized.")
-  v > 2 && println(stdout, S)
-
-  # Initialize SeisEvent structure
-  Ev = SeisEvent(hdr = H, data = S)
-  v > 1 && println(stdout, now(), ": SeisEvent created.")
-  v > 1 && println(stdout, S)
-
-  # Update Ev with distance, azimuth
-  distaz!(Ev)
-  v > 1 && println(stdout, now(), ": Δ,Θ updated.")
-
-  # Add phase arrivals to :data
-  v > 0 && println(stdout, now(), ": phase query begins.")
-  get_pha!(Ev, pha=pha, model=model, to=to, v=v)
-  return Ev
 end
