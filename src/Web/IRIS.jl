1ae1465522e2f2c5772f5fb8999ff2fd735a4036
Joshua Jones <jpjones76@users.noreply.github.com>
2019-06-04 01:50:24 -0700

    moved get_pha to Quake submodule

--- src/Web/IRIS.jl
+++ src/Web/IRIS.jl
@@ -1,4 +1,4 @@
-export get_pha!
+# export get_pha!
 
 function irisws(cha::String, d0::String, d1::String;
                 fmt::String   = KW.fmt,
@@ -94,82 +94,6 @@ function IRISget!(S::SeisData, C::Array{String,1}, d0::String, d1::String;
                   w::Bool       = KW.w)
 
   (parse_err, U) = IRISget(C, d0, d1, fmt = fmt, to = to, opts = opts, v = v, w = w)
-  append!(S,U) # is merge necessary due to scoping? ugh...
+  append!(S,U)
   return parse_err
-end
-
-"""
-    T = get_pha(Δ::Float64, z::Float64)
-
-Command-line interface to IRIS online travel time calculator, which calls TauP [1-2].
-Returns a matrix of strings.
-
-Specify Δ in decimal degrees, z in km treating down as +.
-
-Standard keywords: pha, to, v
-
-Additional keywords:
-* model: velocity model ("iasp91")
-
-### References
-[1] TauP manual: http://www.seis.sc.edu/downloads/TauP/taup.pdf
-[2] Crotwell, H. P., Owens, T. J., & Ritsema, J. (1999). The TauP Toolkit:
-Flexible seismic travel-time and ray-path utilities, SRL 70(2), 154-160.
-"""
-function get_pha!(Ev::SeisEvent;
-                  pha::String   = KW.pha,
-                  model::String = "iasp91",
-                  to::Int64     = KW.to,
-                  v::Int64      = KW.v
-                  )
-
-  # Check that distaz has been done
-  TD = getfield(Ev, :data)
-  N = getfield(TD, :n)
-  z = zeros(Float64, N)
-  if (TD.az == z) && (TD.baz == z) && (TD.dist == z)
-    v > 0 && println(stdout, "az, baz, and dist are unset; calling distaz!...")
-    distaz!(Ev)
-  end
-
-  # Generate URL and do web query
-  src_dep = getfield(getfield(getfield(Ev, :hdr), :loc), :dep)
-  if isempty(pha) || pha == "all"
-    pq = "&phases=ttall"
-  else
-    pq = string("&phases=", pha)
-  end
-  url_tail = string("&evdepth=", src_dep, pq, "&model=", model, "&mintimeonly=true&noheader=true")
-
-  # Loop begins
-  dist = getfield(TD, :dist)
-  PC = getfield(TD, :pha)
-  for i = 1:N
-    Δ = getindex(dist, i)
-    pcat = getindex(PC, i)
-
-    url = string("http://service.iris.edu/irisws/traveltime/1/query?", "distdeg=", Δ, url_tail)
-    v > 1 && println(stdout, "url = ", url)
-
-    req_info_str = string("\nIRIS travel time request:\nΔ = ", Δ, "\nDepth = ", z, "\nPhases = ", pq, "\nmodel = ", model)
-    (R, parsable) = get_HTTP_req(url, req_info_str, to)
-
-    # Parse results
-    if parsable
-      req = String(take!(copy(IOBuffer(R))))
-      pdat = split(req, '\n')
-      deleteat!(pdat, findall(isempty, pdat))   # can have trailing blank line
-      npha = length(pdat)
-      for j = 1:npha
-        pha = split(pdat[j], keepempty=false)
-        pcat[pha[10]] = SeisPha(parse(Float64, pha[8]),
-                                parse(Float64, pha[4]),
-                                parse(Float64, pha[5]),
-                                parse(Float64, pha[6]),
-                                parse(Float64, pha[7]),
-                                ' ')
-      end
-    end
-  end
-  return nothing
 end
