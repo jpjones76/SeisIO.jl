fd6896a4125176b6b96cac9d3a11781225e12221
Joshua <jpjones76@users.noreply.github.com>
2019-08-02 13:04:37 -0700

    moved Octave-style polynomials functions to CoreUtils
    Former-commit-id: f0ba59b978e5bc36b46d117ca8efc6a9b0649676
    Former-commit-id: 71569579ea4360d5d7d19adba658baeff8310305
    Former-commit-id: fc01fc034470671e9828c79fec71657188d023f5

--- src/Processing/0_processing_utils.jl
+++ src/Processing/0_processing_utils.jl
@@ -27,26 +27,3 @@ function nanfill!(x::Array{T,1}) where T<: Real
   end
   return nothing
 end
-
-# Faster than Polynomials.jl with less memory allocation + single-point type
-# stability; adapted from
-# https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl
-function polyval(p::Array{T,1}, x::Array{T,1}) where T <: Real
-  y = last(p)
-  for i = lastindex(p)-1:-1:1
-    y = p[i] .+ x*y
-  end
-  return y
-end
-
-function polyfit(x::Array{T,1}, y::Array{T,1}, n::Int=1) where T <: Real
-  nx = length(x)
-  nx == length(y) || throw(DomainError)
-  1 <= n <= nx - 1 || throw(DomainError)
-  A = Array{T}(undef, length(x), n+1)
-  A[:,1] .= one(T)
-  for i = 1:n
-      A[:,i+1] .= A[:,i] .* x
-  end
-  return A \ y
-end
