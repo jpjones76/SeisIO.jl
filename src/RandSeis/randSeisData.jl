777b04979ac0b332d808422826494e47ca2fa441
Joshua <jpjones76@users.noreply.github.com>
2019-08-02 13:12:10 -0700

    fixed behavior and tooltips for randSeisData keywords s, c
    Former-commit-id: 7120a6bb734b7001a030c82204126eca9fc1cda0
    Former-commit-id: a145b6c4d0ad79ba32a27a7e09ddfb83feec8f63
    Former-commit-id: a78f7063edc51cb46a612f4b5fe2b2f23428136e

--- src/RandSeis/randSeisData.jl
+++ src/RandSeis/randSeisData.jl
@@ -1,23 +1,46 @@
 # Populate a SeisData structure with random channels
 """
-    randSeisData([, c=0.2, s=0.6])
+    randSeisData([, c=0.2::Float64, s=0.6::Float64, nx=L::Integer])
 
 Generate 8 to 24 channels of random seismic data as a SeisData object.
-* 100*c is the percentage of channels _after the first_ with irregularly-sampled data (fs = 0.0)
-* 100*s is the percentage of channels _after the first_ with guaranteed seismic data.
 
-    randSeisData(N[, c=0.2, s=0.6])
+    randSeisData(N, [, c=0.2::Float64, s=0.6::Float64, nx=L::Integer])
 
-Generate N channels of random seismic data as a SeisData object.
+Generate `N` channels of random seismic data as a SeisData object.
+
+### Notes and Keyword Behavior
+* `nx=N` sets the length of each channel to `N` samples.
+* 100*s is the minimum percentage of channels with guaranteed seismic data
+* 100*c is the maximum percentage of channels with irregularly-sampled data
+* `s` takes precedence over `c`; they are not renormalized. Thus, for example:
+  + randSeisData(12, c=1.0, s=1.0) works like randSeisData(12, c=0.0, s=1.0)
+  + randSeisData(c=1.0) works like randSeisData(s=0.0)
+* For `N` channels, `s*N` is rounded up; `c*N` is rounded down. Thus:
+  + randSeisData(10, c=0.28) and randSeisData(10, c=0.2) are equivalent
+  + randSeisData(10, s=0.28) and randSeisData(10, s=0.3) are equivalent
 """
-function randSeisData(N::Int64; c::Float64=0.2, s::Float64=0.6, nx::Int64=0)
+function randSeisData(N::Int64; c::Float64=0.2, s::Float64=0.6, nx::Integer=0)
   S = SeisData(N)
-  n_seis = max(min(ceil(Int, s*S.n), S.n-1),0)
-  n_irr = max(min(floor(Int, c*S.n), S.n-n_seis-1),0)
+
+  # Evaluate probabilities, with s taking precedence
+  if s != 0.6
+    c = max(0.0, 1.0-s)
+  end
+  if c != 0.2
+    s = max(0.0, 1.0-c)
+  end
+
+  # determine number of channels of each
+  n_seis = max(min(ceil(Int, s*S.n), S.n), 0)
+  n_irr = max(min(floor(Int, c*S.n), S.n-n_seis), 0)
+
+  # fill in and shuffle data_spec
   data_spec = zeros(UInt8, S.n)
   data_spec[1:n_seis] .= 0x01
   data_spec[n_seis+1:n_seis+n_irr] .= 0x02
-  data_spec = shuffle!(data_spec)
+  shuffle!(data_spec)
+
+  # populate all channels according to data_spec
   for i = 1:S.n
     if data_spec[i] == 0x01
       S[i] = randSeisChannel(s=true, nx=nx)
