cda95e6d4250b7b55e4919c3c460c260dadc32e4
Joshua <jpjones76@users.noreply.github.com>
2019-08-02 13:08:14 -0700

    now accepts ranges of channels
    Former-commit-id: 978aa275d61f3e8f00f286b7ad7ebdf6bade842a
    Former-commit-id: 042e8f896f11f15bd116cf330218e637b968b41b
    Former-commit-id: 50fa81d93624eccf202b6889a9eee338cc1b8f3f

--- src/Utils/get_unique.jl
+++ src/Utils/get_unique.jl
@@ -4,14 +4,17 @@
 # X, a set of views corresponding to each H
 # Get sets of channels where all channels in inds[i] have matching properties
 # as specified in A
-function get_unique(S::T, A::Array{String,1}) where {T<:GphysData}
+function get_unique(S::T, A::Array{String,1}; chans::Union{Integer, UnitRange, Array{Int64,1}}=Int64[]) where {T<:GphysData}
+  if chans == Int64[]
+    chans = 1:S.n
+  end
   J = lastindex(A)
   N = getfield(S, :n)
-  H = Array{UInt64,1}(undef,N)
+  H = Array{UInt64,1}(undef, length(chans))
   fields = fieldnames(T)
   h = Array{UInt64,1}(undef, J)
   cmd = Array{String,1}(undef, J)
-  @inbounds for i = 1:N
+  @inbounds for (c,i) in enumerate(chans)
     for (j, str) in enumerate(A)
       sym = Symbol(str)
       if any(fields.==sym)
@@ -20,26 +23,26 @@ function get_unique(S::T, A::Array{String,1}) where {T<:GphysData}
         h[j] = hash(getfield(Main, sym)(getfield(S, :x)[i]))
       end
     end
-    H[i] = hash(h)
+    H[c] = hash(h)
   end
 
   # Get unique hashes
   Uh = unique(H)
   Nh = length(Uh)
-  inds = Array{Array{Int64,1},1}(undef,Nh)
+  inds = Array{Array{Int64,1},1}(undef, Nh)
 
   @inbounds for n = 1:Nh
     inds[n] = Array{Int64,1}(undef,0)
-    for i = 1:N
+    for (c,i) in enumerate(chans)
       # Uses an order of magnitude less memory than findall
-      if H[i] == Uh[n]
+      if H[c] == Uh[n]
         push!(inds[n], i)
         continue
       end
     end
   end
 
-  # One last pass...sort by size of eltype(S.x) in descending order
+  # One last pass...sort by size of eltype(S.x[i[1]]) in descending order
   el_size = [sizeof(eltype(S.x[i[1]])) for i in inds]
   ii = sortperm(el_size, rev=true)
   inds = inds[ii]
