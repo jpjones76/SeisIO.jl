3a723ab95c36b680d809afe9f79e600503e97bb8
Joshua Jones <jpjones76@users.noreply.github.com>
2019-08-06 01:08:51 -0700

    added inst_codes
    Former-commit-id: 1c7cd542793b015e51988269be67038e469a68c6
    Former-commit-id: 8df342675ea7b560477ddb8912f4d982fda71a03
    Former-commit-id: 27e26eb8ffb7b019243c48fbd958d81b4a3d0a51

---
+++ src/Utils/inst_codes.jl
@@ -0,0 +1,72 @@
+export inst_codes, inst_code
+
+@doc """
+    inst_codes(S::GphysData)
+
+Get the instrument code of each channel in `S`.
+
+    inst_code(S::GphysData, i::Integer)
+
+Get the instrument code of channel `i`.
+
+inst_code(C::GphysChannel)
+
+Get the instrument code from `C.id`.
+
+Assumes each ID ends with an alphanumeric three-digit channel code and that
+ID fields are separated by periods: for example, two channels with IDs
+"XX.YYY.00.EHZ" and "_.YHY" each have an instrument code of 'H'.
+
+Channel codes less than two characters long (e.g. "Z" in "AA.BBB.CC.Z") are ignored.
+
+SEED channel codes of seismic and seismoacoustic data (for which operations
+like `detrend!` and `taper!` are sane) include D, G, H, J, L, M, N, P, Z.
+
+SEED channel codes of seismometers (for which `translate_resp!` and
+`remove_resp!` are sane) are H, J, L, M, N, P, Z.
+""" inst_codes
+function inst_codes(S::GphysData)
+  N = S.n
+  codes = Array{Char, 1}(undef, N)
+  fill!(codes, '\0')
+  @inbounds for i = 1:N
+    id = S.id[i]
+    L = length(id)
+    for j = L:-1:1
+      if id[j] == '.'
+        j > L-2 && break
+        setindex!(codes, id[j+2], i)
+        break
+      end
+    end
+  end
+  return codes
+end
+
+@doc (@doc inst_codes)
+function inst_code(S::GphysData, i::Integer)
+  N = S.n
+  id = S.id[i]
+  L = length(id)
+  for j = L:-1:1
+    if id[j] == '.'
+      j > L-2 && break
+      return id[j+2]
+    end
+  end
+  return '\0'
+end
+
+@doc (@doc inst_codes)
+function inst_code(C::GphysChannel)
+  N = S.n
+  id = S.id[i]
+  L = length(id)
+  for j = L:-1:1
+    if id[j] == '.'
+      j > L-2 && break
+      return id[j+2]
+    end
+  end
+  return '\0'
+end
