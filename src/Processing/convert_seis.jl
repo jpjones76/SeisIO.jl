4c84c9a642343d2851621b870d67f7b0ec237751
Joshua Jones <jpjones76@users.noreply.github.com>
2019-08-13 22:36:43 -0700

    add convert_seis

---
+++ src/Processing/convert_seis.jl
@@ -0,0 +1,209 @@
+export convert_seis, convert_seis!
+
+@doc """
+    convert_seis!(S[, chans=CC, units_out=UU, v=V])
+    convert_seis(S, chans=CC, units_out=UU, v=V])
+    convert_seis!(C[, units_out=UU, v=V])
+    convert_seis(CC, units_out=UU, v=V)
+
+    or integrating as needed. Integration is rectangular via. Neumaier's algorithm[^1].
+Convert all seismic data channels in `S` to velocity seismograms, differentiating
+
+### Keywords
+* `units_out=UU` specifies output units.
+  + Default: "m/s".
+  + Allowed: "m", "m/s", or "m/s²". (SeisIO uses Unicode (UTF-8) SI units.)
+* `v=V` sets verbosity.
+* `chans=CC` restricts seismogram conversion to seismic data channels within `CC`.
+  + `chans` can be an Integer, UnitRange, or Array{Int64,1}.
+  + By default, all seismic data channels in `S` are converted (if needed).
+  + This does not allow `convert_seis!` to work on non-seismic data.
+
+!!! warning
+
+    `convert_seis!` becomes less reversible as seismograms lengthen, particularly at Float32 precision.
+
+### References
+[^1] Neumaier, A. (1974). "Rundungsfehleranalyse einiger Verfahren zur Summation
+endlicher Summen" [Rounding Error Analysis of Some Methods for Summing Finite
+Sums]. Zeitschrift für Angewandte Mathematik und Mechanik (in German). 54 (1):
+39–51. doi:10.1002/zamm.19740540106.
+
+""" convert_seis!
+function convert_seis!(S::GphysData;
+  chans::Union{Integer, UnitRange, Array{Int64,1}}=Int64[],
+  units_out::String="m/s",
+  v::Int64=KW.v)
+
+  # sanity check
+  if units_out in ("m", "m/s", "m/s²") == false
+    error("units_out must be in (\"m\", \"m/s\", \"m/s²\")!")
+  end
+
+  # get seismic data channels
+  chans = mkchans(chans, S.n)
+  filt_seis_chans!(chans, S)
+
+  # now loop over all seismic channels
+  for i in chans
+    if v > 2
+      println(stdout, "Begin channel ", i)
+    end
+
+    # get units
+    units_in = lowercase(S.units[i])
+    units_in == units_out && continue
+
+    # differentiate or integrate
+    if v > 0
+      println(stdout, "Converting channel ", i, ": ", units_in, " => ", units_out)
+    end
+
+    T = eltype(S.x[i])
+    fs = T(S.fs[i])
+    δ = one(T)/fs
+    dt = round(Int64, 1.0/(S.fs[i]*μs))
+    d2 = div(dt, 2)
+    t = S.t[i]
+    Nt = size(t, 1) - 1
+
+    if units_out == "m/s"
+
+      # differentiate from m to m/s
+      if units_in == "m"
+        diff_x!(S.x[i], t[:,1], fs)
+        for j = 1:Nt
+          t[j,2] -= d2
+        end
+
+      # integrate from m/s² to m/s
+      else
+        int_x!(S.x[i], t[:,1], δ)
+        for j = 1:Nt
+          t[j,2] -= d2
+        end
+      end
+
+    elseif units_out == "m"
+      int_x!(S.x[i], t[:,1], δ)
+      for j = 1:Nt
+        t[j,2] -= d2
+      end
+      if units_in == "m/s²"
+        int_x!(S.x[i], t[:,1], δ)
+        for j = 1:Nt
+          t[j,2] -= d2
+        end
+      end
+    else # units == "m/s²"
+      diff_x!(S.x[i], t[:,1], fs)
+      for j = 1:Nt
+        t[j,2] += d2
+      end
+      if units_in == "m"
+        diff_x!(S.x[i], t[:,1], fs)
+        for j = 1:Nt
+          t[j,2] += d2
+        end
+      end
+    end
+
+    # change units
+    S.units[i] = units_out
+
+    # log processing
+    note!(S, i, string("convert_seis!, units_old = ", units_in))
+
+    if v > 2
+      println(stdout, "Done channel ", i)
+    end
+  end
+
+  return nothing
+end
+
+function convert_seis!(C::GphysChannel;
+  units_out::String="m/s",
+  v::Int64=KW.v)
+
+  # sanity check
+  if units_out in ("m", "m/s", "m/s²") == false
+    error("units_out must be in (\"m\", \"m/s\", \"m/s²\")!")
+  end
+
+  units_in = lowercase(C.units)
+  units_in == units_out && return nothing
+
+  # differentiate or integrate
+  if v > 0
+    println(stdout, "Converting ", units_in, " => ", units_out)
+  end
+
+  T = eltype(C.x)
+  fs = T(C.fs)
+  δ = one(T)/fs
+  dt = round(Int64, 1.0/(C.fs*μs))
+  d2 = div(dt, 2)
+  t = C.t
+  Nt = size(t, 1) - 1
+
+  if units_out == "m/s"
+
+    # differentiate from m to m/s
+    if units_in == "m"
+      diff_x!(C.x, t[:,1], fs)
+      for j = 1:Nt
+        t[j,2] -= d2
+      end
+
+    # integrate from m/s² to m/s
+    else
+      int_x!(C.x, t[:,1], δ)
+      for j = 1:Nt
+        t[j,2] -= d2
+      end
+    end
+
+  elseif units_out == "m"
+    int_x!(C.x, t[:,1], δ)
+    for j = 1:Nt
+      t[j,2] -= d2
+    end
+    if units_in == "m/s²"
+      int_x!(C.x, t[:,1], δ)
+      for j = 1:Nt
+        t[j,2] -= d2
+      end
+    end
+  else # units == "m/s²"
+    diff_x!(C.x, t[:,1], fs)
+    for j = 1:Nt
+      t[j,2] += d2
+    end
+    if units_in == "m"
+      diff_x!(C.x, t[:,1], fs)
+      for j = 1:Nt
+        t[j,2] += d2
+      end
+    end
+  end
+
+  # change units
+  C.units = units_out
+
+  # log processing
+  note!(C, string("convert_seis!, units_old = ", units_in))
+
+  return nothing
+end
+
+@doc (@doc convert_seis!)
+function convert_seis(S::GphysData;
+  chans::Union{Integer, UnitRange, Array{Int64,1}}=Int64[],
+  units_out::String="m/s",
+  v::Int64=KW.v)
+
+  U = deepcopy(S)
+  convert_seis!(U, chans=chans, units_out=units_out, v=v)
+  return U
+end
