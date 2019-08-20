ffa9d2119bf68614167fe125a4bf7d37e190ee76
Joshua Jones <jpjones76@users.noreply.github.com>
2019-08-19 20:06:37 -0700

    test improvements

---
+++ test/Processing/test_env.jl
@@ -0,0 +1,37 @@
+printstyled("  env!\n", color=:light_green)
+
+# GphysChannel, gaps
+printstyled("    on SeisChannel\n", color=:light_green)
+C = randSeisChannel(s=true)
+while size(C.t, 1) < 3
+  println("rerolling")
+  C = randSeisChannel(s=true)
+end
+env!(C)
+
+# GphysChannel, no gaps
+C = randSeisChannel(s=true)
+ungap!(C)
+D = env(C)
+
+# GphysData
+printstyled("    on SeisData\n", color=:light_green)
+S = randSeisData(24)
+ungap!(S)
+U = deepcopy(S)
+env!(S, v=2)
+printstyled("    testing that env! == DSP.hilbert\n", color=:light_green)
+for i = 1:S.n
+  if S.fs[i] > 0.0
+    ex1 = S.x[i]
+    ex2 = abs.(DSP.hilbert(U.x[i]))
+    @test isapprox(S.x[i], abs.(DSP.hilbert(U.x[i])))
+  end
+end
+
+printstyled("    testing that smoothing works\n", color=:light_green)
+S = randSeisData(24)
+U = deepcopy(S)
+Sy = deepcopy(U)
+demean!(Sy)
+Sm = env(Sy, smooth=true)
