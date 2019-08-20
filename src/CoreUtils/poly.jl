e11bd552a07fe0f3b309663fd4374812c49a4b2b
Joshua Jones <jpjones76@users.noreply.github.com>
2019-08-19 20:05:11 -0700

    added linreg, which should really not be in this file
    Former-commit-id: d87a82ff6b803a9f355e1b0f89342930b4752d34
    Former-commit-id: 6bd848dc4d22f22b67a6058cc0280af7f913bb77
    Former-commit-id: 8b6e3a188eab472a2dfe0ea3f6335d80c659228a

--- src/CoreUtils/poly.jl
+++ src/CoreUtils/poly.jl
@@ -1,47 +1,3 @@
-# # We only really want the polynomial
-# function poly(x::Array{T,1}) where T <: Number
-#   n = length(x)
-#   y = zeros(T, n+1)
-#   y[1] = one(T)
-#   for j = 1:n
-#     y[2:j+1] .-= x[j].*y[1:j]
-#   end
-#   return reverse(y)
-# end
-#
-# # Faster than Polynomials.jl with less memory allocation + single-point type
-# # stability; adapted from
-# # https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl
-# function polyval(p::Array{T,1}, x::T) where T <: Number
-#   y = one(T)
-#   for i = lastindex(p)-1:-1:1
-#     y = p[i] .+ x*y
-#   end
-#   return y
-# end
-#
-# function polyval(p::Array{T,1}, x::Array{T,1}) where T <: Number
-#   y = ones(T, length(x)) .* first(p)
-#   for i = 2:length(p)
-#     broadcast!(*, y, y, x)
-#     broadcast!(+, y, y, p[i])
-#   end
-#   return y
-# end
-#
-# function polyfit(x::Array{T,1}, y::Array{T,1}, n::Int=1) where T <: Real
-#   nx = length(x)
-#   nx == length(y) || throw(DomainError)
-#   1 <= n <= nx - 1 || throw(DomainError)
-#   A = Array{T}(undef, length(x), n+1)
-#   A[:,1] .= one(T)
-#   for i = 1:n
-#       A[:,i+1] .= A[:,i] .* x
-#   end
-#   return reverse(A \ y)
-# end
-
-
 # Faster than Polynomials.jl with less memory allocation + single-point type
 # stability; adapted from Octave and
 # https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl
@@ -55,16 +11,16 @@ function poly(x::Array{T,1}) where T <: Number
   return y
 end
 
-function polyval(p::Array{T,1}, x::T) where T <: Number
-  y = p[1]
+function polyval(p::Array{T1,1}, x::T2) where {T1 <: Number, T2 <: Number}
+  y = T2(p[1])
   for i = 2:lastindex(p)
     y = p[i] .+ x*y
   end
   return y
 end
 
-function polyval(p::Array{T,1}, x::Array{T,1}) where T <: Number
-  y = ones(T, length(x)) .* p[1]
+function polyval(p::Array{T1,1}, x::Array{T2,1}) where {T1 <: Number, T2 <: Number}
+  y = ones(T2, length(x)) .* p[1]
   for i = 2:length(p)
     broadcast!(*, y, y, x)
     broadcast!(+, y, y, p[i])
@@ -72,23 +28,54 @@ function polyval(p::Array{T,1}, x::Array{T,1}) where T <: Number
   return y
 end
 
-function polyfit(x::Array{T,1}, y::Array{T,1}, n::Integer=1) where T <: Real
+function polyfit(x::Array{T1,1}, y::Array{T2,1}, n::Integer=1) where {T1 <: Real, T2 <: Real}
   nx = length(x)
   nx == length(y) || error("SeisIO.polyfit requires length(t) == length(x)")
   -1 < n < nx || throw(DomainError)
-  A = Array{T, 2}(undef, length(x), n+1)
-  A[:,n+1] .= one(T)
+  A = Array{T2, 2}(undef, length(x), n+1)
+  A[:,n+1] .= one(T2)
   for i = n:-1:1
       A[:,i] .= A[:,i+1] .* x
   end
   return A \ y
 end
 
+# Convert to Float64 or use improved sum
+function linreg(t::Array{Float64,1}, x::AbstractArray{T,1}) where T
+  n = length(t)
+  st = sum(t)
+  sx = sum(x)
+  stt = dot(t,t)
+  stx = dot(t,x)
+  sxx = dot(x,x)
+  d = n*stt - st*st
+  b = (stt*sx - st*stx)/d
+  a = (n*stx - st*sx)/d
+  return T[a,b]
+end
+
+# p = linreg(x, fs)
+function linreg(x::AbstractArray{T,1}, dt::Float64) where T
+  n = length(x)
+  t = dt:dt:n*dt
+  st = sum(t)
+  sx = sum(x)
+  stt = dot(t,t)
+  stx = dot(t,x)
+  sxx = dot(x,x)
+  d = n*stt - st*st
+  b = (stt*sx - st*stx)/d
+  a = (n*stx - st*sx)/d
+  return T[a,b]
+end
+
 #= CHANGELOG for this file
 2019-08-08
 * poly, polyval, and polyfit should now always output powers in descending
 order, i.e., p^n ... p^0
-* BUG fixed: polyval(p::Array{T,1}, x::T) used the power order of Polynomials.jl;
+* BUG fixed: polyval(p::Array{T,1}, x::T) used power ordering of Polynomials.jl;
 corrected to be consistent with other routines
 * polyfit now allows order n=0 and takes any Integer for n; n=0 returns the mean
+2019-08-19
+* added linreg for low-memory linear regression; identical to SAC detrend
 =#
