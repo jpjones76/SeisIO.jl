61d3b49e97c56c082fcbd625f01a9a45c40b1772
Joshua Jones <jpjones76@users.noreply.github.com>
2019-08-23 00:40:23 -0700

    moved to src/Submodules/

--- src/Submodules/Quake.jl
+++ src/Submodules/Quake.jl
@@ -7,44 +7,43 @@ path = Base.source_dir()
 
 const tracefields = (:az, :baz, :dist, :id, :loc, :fs, :gain, :misc, :name, :notes, :pha, :resp, :src, :t, :units, :x)
 
-
 # imports
-include("imports.jl")
+include("Quake/imports.jl")
 
 # types for earthquake data
-include("Types/EQLoc.jl")
-include("Types/EQMag.jl")
-include("Types/SourceTime.jl")
-include("Types/SeisSrc.jl")
-include("Types/SeisPha.jl")
-include("Types/PhaseCat.jl")
-include("Types/SeisHdr.jl")
-include("Types/EventTraceData.jl")
-include("Types/EventChannel.jl")
-include("Types/SeisEvent.jl")
+include("Quake/Types/EQLoc.jl")
+include("Quake/Types/EQMag.jl")
+include("Quake/Types/SourceTime.jl")
+include("Quake/Types/SeisSrc.jl")
+include("Quake/Types/SeisPha.jl")
+include("Quake/Types/PhaseCat.jl")
+include("Quake/Types/SeisHdr.jl")
+include("Quake/Types/EventTraceData.jl")
+include("Quake/Types/EventChannel.jl")
+include("Quake/Types/SeisEvent.jl")
 
 # extensions
-for i in ls(path*"/Ext/*")
+for i in ls(path*"/Quake/Ext/*")
   if endswith(i, ".jl")
     include(i)
   end
 end
 
 # utilities
-for i in ls(path*"/Utils/*")
+for i in ls(path*"/Quake/Utils/*")
   if endswith(i, ".jl")
     include(i)
   end
 end
 
 # web
-for i in ls(path*"/Web/*")
+for i in ls(path*"/Quake/Web/*")
   if endswith(i, ".jl")
     include(i)
   end
 end
 
 # exports
-include("exports.jl")
+include("Quake/exports.jl")
 
 end
--- src/Submodules/RandSeis.jl
+++ src/Submodules/RandSeis.jl
@@ -6,10 +6,10 @@ import SeisIO:InstrumentPosition, code2typ, getbandcode, note!, sμ, μs
 export randSeisChannel, randSeisData, randSeisEvent, randSeisHdr, randPhaseCat,
 randSeisSrc
 
-include("randseis_utils.jl")
-include("randSeisChannel.jl")
-include("randSeisData.jl")
-include("randSeisEvent.jl")
+include("RandSeis/randseis_utils.jl")
+include("RandSeis/randSeisChannel.jl")
+include("RandSeis/randSeisData.jl")
+include("RandSeis/randSeisEvent.jl")
 
 const fc_vals = Float64[1.0/120.0 1.0/60.0 1.0/30.0 0.2 1.0 1.0 1.0 2.0 4.5 15.0]
 const fs_vals = Float64[0.1, 1.0, 2.0, 5.0, 10.0, 20.0, 25.0, 40.0, 50.0, 60.0, 62.5, 80.0, 100.0, 120.0, 125.0, 250.0]
