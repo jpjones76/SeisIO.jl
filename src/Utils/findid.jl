b4658763a4a8bc2738837d2574a833fdc63c50a9
Joshua <jpjones76@users.noreply.github.com>
2019-08-27 02:26:53 -0700

    added method findid(id::Array{UInt8,1}, ID::Array{String,1})

--- src/Utils/findid.jl
+++ src/Utils/findid.jl
@@ -8,3 +8,14 @@ function findid(id::String, ID::Array{String,1})
   end
   return c
 end
+
+function findid(id::Array{UInt8,1}, ID::Array{String,1})
+  c = 0
+  for i = 1:length(ID)
+    if codeunits(ID[i]) == id
+      c = i
+      break
+    end
+  end
+  return c
+end
