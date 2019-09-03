f5d1d5221599336bfc0e73d5daef915ce9a4006b
Joshua <jpjones76@users.noreply.github.com>
2019-09-03 14:28:10 -0700

    typo

--- src/CoreUtils/mkchans.jl
+++ src/CoreUtils/mkchans.jl
@@ -4,7 +4,7 @@ function mkchans(chans::Union{Integer, UnitRange, Array{Int64,1}}, n::Int64)
 
   if chans == Int64[]
     return Int64.(collect(1:n))
-  elseif typeof(chans) == UnitRange
+  elseif typeof(chans) <: UnitRange
     return Int64.(collect(chans))
   elseif typeof(chans) <: Integer
     return [Int64(chans)]
