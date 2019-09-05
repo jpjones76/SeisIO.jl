b904a9b516075c8438aa97655eb821ca514ef50b
Joshua Jones <jpjones76@users.noreply.github.com>
2019-09-05 11:28:01 -0700

    code cleanup; trivial

--- src/imports.jl
+++ src/imports.jl
@@ -1,5 +1,6 @@
 import Base: ==, -, *, +, append!, convert, copy, delete!, deleteat!,
-  firstindex, getindex, hash, in, isempty, isequal, lastindex, length, push!,
-  merge, merge!, read, setindex!, show, size, sizeof, sort!, sort, summary, write
+  firstindex, getindex, hash, in, isempty, isequal, lastindex, length, 
+  push!, merge, merge!, read, setindex!, show, size, sizeof, sort!,
+  sort, summary, write
 import Base.Libc: TmStruct
 import DSP: filtfilt
