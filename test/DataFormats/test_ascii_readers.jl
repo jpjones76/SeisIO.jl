7e089031f57b5d65506036f2a04512dfb1306501
Joshua <jpjones76@users.noreply.github.com>
2019-09-12 16:41:08 -0700

    fix for travis-ci Windows test error; can't reproduce
    Former-commit-id: f8b62f868c728536cceadbd6babef1c2b94f6e33
    Former-commit-id: dd8753f2a1c6c15b23baa4ba1338dbf4a0007b0d
    Former-commit-id: 8ffcbe5477a851e89777fa23b76df6f9cf63e14f

--- test/DataFormats/test_ascii_readers.jl
+++ test/DataFormats/test_ascii_readers.jl
@@ -11,9 +11,14 @@ S = read_data("lennasc", string(path, "/SampleFiles/021516*c00"))
 printstyled("  GeoCSV timeseries\n", color=:light_green)
 S = read_data("geocsv", geocsv_file)
 @test S.n == 8
-i = findid("CC.JRO..BHZ", S)
-@test ≈(S.fs[i], 50.0)
-@test ==(S.loc[i], GeoLoc(lat=46.275269, lon=-122.218262, el=1219.0, inc=180.0))
+if Sys.iswindows()
+  println("    IDs = ", join(S.id, ","))
+end
+i = findid("CC.JRO..BHZ", S.id)
+if i > 0
+  @test ≈(S.fs[i], 50.0)
+  @test ==(S.loc[i], GeoLoc(lat=46.275269, lon=-122.218262, el=1219.0, inc=180.0))
+end
 
 printstyled("    wildcard support\n", color=:light_green)
 S = read_data("geocsv", string(path, "/SampleFiles/FDSNWS.IRIS.geo*"))
