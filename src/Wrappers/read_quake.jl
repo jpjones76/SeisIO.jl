f576987e69c9576410a1b043a43ca8dcd2f44c73
Joshua Jones <jpjones76@users.noreply.github.com>
2019-09-19 20:34:05 -0700

    add read_quake.jl
    Former-commit-id: 81546c949541ba2c17b6c2008289726d2c9051f0
    Former-commit-id: 94b843b8d176b34b78fd426ed3f0c7401a3f7613
    Former-commit-id: 4a96e0a1f33e684cc2851f918f5c3e87d9914ba7

---
+++ src/Wrappers/read_quake.jl
@@ -0,0 +1,45 @@
+export read_quake
+
+@doc """
+    Ev = read_quake(fmt, file [, keywords])
+
+Read data in file format `fmt` from `file` into SeisEvent object `Ev`.
+
+| Format      | String          | Notes                                       |
+
+|:---         |:---             |:----                                        |
+| PC-SUDS     | suds            |                                             |
+| QuakeML     | qml, quakeml    | only reads first event from file            |
+| UW          | uw              |                                             |
+
+
+|KW       | Used By   | Type    | Default   | Meaning                         |
+|:---     |:---       |:---     |:---       |:---                             |
+| full    | suds, uw  | Bool    | false     | read full header into `:misc`?  |
+| v       | all       | Int64   | 0         | verbosity                       |
+
+### Notes on Functionality
+* No "in-place" version of `read_quake` exists because earthquake data are
+usually discrete, self-contained files.
+* `read_quake` doesn't use file wildcards. See `?UW.readuwevt` for help with UW
+file string syntax.
+
+See also: read_data, get_data, read_meta
+""" read_quake!
+function read_quake(fmt::String, fname::String;
+  full    ::Bool    = false,              # full header
+  v       ::Int64   = KW.v                # verbosity level
+  )
+
+  Ev = (
+    if fmt == "suds"
+      SUDS.readsudsevt(fname, full=full, v=v)
+    elseif fmt == "uw"
+      UW.readuwevt(fname, full=full, v=v)
+    elseif fmt in ("qml", "quakeml")
+      hdr, source = read_qml(fname)
+      SeisEvent(hdr = hdr[1], source = source[1])
+    end
+    )
+  return Ev
+end
