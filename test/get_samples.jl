2365e7341ff427b1f789e28a2eae008555975a98
Joshua Jones <jpjones76@users.noreply.github.com>
2019-10-02 15:49:08 -0700

    test svn for Appveyor
    Former-commit-id: 834ae19b05d5fe5c477ef471824f3cc7a3d5803d
    Former-commit-id: 9a1faa01dafee178c34eda6f599d27190e60be3c
    Former-commit-id: b3f4e1d69578f6a438a56b4eab63e553bd6e41ca

--- test/get_samples.jl
+++ test/get_samples.jl
@@ -1,14 +1,20 @@
 function get_SampleFiles()
   status = 1
   if Sys.iswindows()
-    p = run(`cmd /c svn export https://github.com/jpjones76/SeisIO-TestData/trunk/SampleFiles SampleFiles`)
-    p.exitcode
+    status = (try
+      p = run(`cmd /c svn export https://github.com/jpjones76/SeisIO-TestData/trunk/SampleFiles SampleFiles`)
+      0
+    catch err
+      @warn(string("svn threw error: ", err))
+      1
+    end)
   else
     status = (try
       p = run(`svn export https://github.com/jpjones76/SeisIO-TestData/trunk/SampleFiles SampleFiles`)
       p.exitcode
     catch err
       @warn(string("svn threw error: ", err))
+      println()
       1
     end)
   end
