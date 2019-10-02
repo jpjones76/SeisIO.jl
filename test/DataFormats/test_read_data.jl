c5689d84fe77f2257415cf2d1520ce23e972dc78
Joshua Jones <jpjones76@users.noreply.github.com>
2019-10-02 16:50:28 -0700

    windows is bad at syntax
    Former-commit-id: df408a0457b35c37ce12f95892bdf2f47a48f588
    Former-commit-id: a6dc0c8fcc6a0764e0659aa7e60eceebb5cb6e0a
    Former-commit-id: dccf86c68e05abe0d4af88c0985c3daa91b58e69

--- test/DataFormats/test_read_data.jl
+++ test/DataFormats/test_read_data.jl
@@ -45,37 +45,38 @@ printstyled("  read_data\n", color=:light_green)
 nf = size(files,1)
 for n = 1:nf
   fname = pref * files[n,1]
-  (occursin("Restricted", fname) && (has_restricted==false)) && continue
-  fwild = fname[1:end-1] * "*"
-  f_call = files[n,2]
-  opt = files[n,3]
-  printstyled(string("    ", n, "/", nf, " ", f_call, "\n"), color=:light_green)
-  if opt == "pa-full"
-    S = read_data("passcal", fname, full=true)
-    S = read_data("passcal", fwild, full=true)
-    S = read_data(fname)
-    S = read_data(fname, full=true)
-  elseif opt == "win"
-    S = read_data(f_call, fwild, cf=cfile)
-    S = read_data(fwild, cf=cfile)
-  elseif opt == "slist"
-    S = read_data("geocsv.slist", fname)
-    S = read_data("geocsv.slist", fwild)
-    S = read_data(fwild)
-  elseif opt == "lo-mem"
-    S = read_data(f_call, fname, nx_new=nx_new, nx_add=nx_add)
-    S = read_data(f_call, fwild, nx_new=nx_new, nx_add=nx_add)
-  elseif opt == "full"
-    S = read_data(f_call, fname, full=true)
-    S = read_data(f_call, fwild, full=true)
-    S = read_data(fwild, full=true)
-  else
-    S = read_data(f_call, fname)
-    if f_call == "uw"
-      fwild = fname[1:end-3]*"*"*"W"
+  if (occursin("Restricted", fname)==false || (has_restricted==true))
+    fwild = fname[1:end-1] * "*"
+    f_call = files[n,2]
+    opt = files[n,3]
+    printstyled(string("    ", n, "/", nf, " ", f_call, "\n"), color=:light_green)
+    if opt == "pa-full"
+      S = read_data("passcal", fname, full=true)
+      S = read_data("passcal", fwild, full=true)
+      S = read_data(fname)
+      S = read_data(fname, full=true)
+    elseif opt == "win"
+      S = read_data(f_call, fwild, cf=cfile)
+      S = read_data(fwild, cf=cfile)
+    elseif opt == "slist"
+      S = read_data("geocsv.slist", fname)
+      S = read_data("geocsv.slist", fwild)
+      S = read_data(fwild)
+    elseif opt == "lo-mem"
+      S = read_data(f_call, fname, nx_new=nx_new, nx_add=nx_add)
+      S = read_data(f_call, fwild, nx_new=nx_new, nx_add=nx_add)
+    elseif opt == "full"
+      S = read_data(f_call, fname, full=true)
+      S = read_data(f_call, fwild, full=true)
+      S = read_data(fwild, full=true)
+    else
+      S = read_data(f_call, fname)
+      if f_call == "uw"
+        fwild = fname[1:end-3]*"*"*"W"
+      end
+      S = read_data(f_call, fwild)
+      S = read_data(fwild)
     end
-    S = read_data(f_call, fwild)
-    S = read_data(fwild)
   end
 end
 
