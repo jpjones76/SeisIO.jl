e94ead02bbca75e97e630500b87f31cd92f333fc
Joshua Jones <jpjones76@users.noreply.github.com>
2019-10-02 14:45:54 -0700

    moved tutorial data to SeisIO-TestFiles
    Former-commit-id: cbf6952731615ca493b2e913aed899c688ef6ff7
    Former-commit-id: 57d51adcb7750313bac71a423b86eb72e19cfa16
    Former-commit-id: 1c98d5e6668e48bf661d38dc4b8a94b79c4771a0

--- tutorial/install.jl
+++ tutorial/install.jl
@@ -1,4 +1,19 @@
 using Pkg
-Pkg.add(["Dates", "DSP", "Plots", "SeisIO", "IJulia"])
+Pkg.add(["Dates", "DSP", "SeisIO", "IJulia"])
 using IJulia
+
+# Check that the tutorial files exist
+tutorial_dir =  dirname(pathof(SeisIO))*"/../tutorial/DATA"
+if isdir(tutorial_dir)
+  println("tutorial/ exists; not downloading.")
+else
+  println("dowloading tutorial data (17 MB)...")
+  if Sys.iswindows()
+    error("NYI")
+  else
+    p = (run(`svn export https://github.com/jpjones76/SeisIO-TestData/trunk/Tutorial DATA`)).exitcode
+    (p == 0) || error("error downloading tutorial/!")
+  end
+end
+
 jupyterlab(dir=pwd())
