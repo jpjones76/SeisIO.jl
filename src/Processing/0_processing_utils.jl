b40c9d6cc4ef9161ba19e695999d7123871729bd
Joshua <jpjones76@users.noreply.github.com>
2019-10-22 15:37:03 -0700

    gapfill optimization; changed behavior for negative gaps

--- src/Processing/0_processing_utils.jl
+++ src/Processing/0_processing_utils.jl
@@ -1,18 +1,46 @@
+"""
+    gapfill!(x t, fs; m::Bool=true)
+    y = gapfill(x, t, fs; m::Bool=true)
+
+Fill gaps in `x`, sampled at `fs`, with gap indices given by `t[:,1]` and
+gap lengths in μs given by `t[:,2]`.
+
+Specify `m=false` to fill with NaNs; else, fill with the mean of non-NaN
+values in `x`.
+"""
 function gapfill!(x::Array{T,1}, t::Array{Int64,2}, fs::Float64; m::Bool=true) where T<: Real
   (fs == 0.0 || isempty(x)) && (return x)
-  mx::T = m ? mean(x[isnan.(x).==false]) : NaN
-  u = round(Int64, max(20, 0.2*fs))
-  for i = size(t,1):-1:2
-    # Gap fill
-    g = round(Int64, fs*μs*t[i,2])
-    g < 0 && (@warn(string("Negative time gap (i = ", i, ", t = ", Float64(g)/fs, "; skipped.")); continue)
-    g == 0 && continue
-    j = t[i-1,1]
-    k = t[i,1]
-    N = k-j
-    splice!(x, k:k-1, mx.*ones(T, g))
+  nt = size(t,1)
+  ng = nt - (t[nt,2] == 0 ? 2 : 1)
+  (ng == 0) && (return x)
+  mx = m ? mean(skipmissing(x)) : T(NaN)
+  u = max(Int64(20), round(Int64, 0.2*fs))
+  Δ = round(Int64, 1.0/(fs*μs))
+  nx = length(x) + div(sum(t[2:end,2]), Δ)
+  resize!(x, nx)
+
+  w = t_win(t, Δ)
+  broadcast!(-, w, w, w[1,1])
+  broadcast!(div, w, w, Δ)
+  broadcast!(+, w, w, 1)
+  nw = size(w,1)
+  for i = nw:-1:1
+    N = w[i,2]-w[i,1]+1
+    j = t[i,1]
+    copyto!(x, w[i,1], x, j, N)
+    if i > 1
+      fill_s = w[i-1,2]+1
+      fill_e = w[i,1]-1
+      x[fill_s:fill_e] .= mx
+    end
   end
   return nothing
+end
+
+function gapfill(x::Array{T,1}, t::Array{Int64,2}, fs::Float64; m::Bool=true) where T<: Real
+  y = deepcopy(x)
+  gapfill!(y, t, fs, m=m)
+  return y
 end
 
 # replace NaNs with the mean
