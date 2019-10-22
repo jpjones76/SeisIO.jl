c18e16e830308a29d748ab27f1190a2f6e61728f
Joshua <jpjones76@users.noreply.github.com>
2019-10-22 15:41:16 -0700

    changed imports, declare constants at submodule begin

--- src/Submodules/SeisHDF.jl
+++ src/Submodules/SeisHDF.jl
@@ -1,13 +1,18 @@
 module SeisHDF
-using Dates, HDF5, SeisIO, SeisIO.XMLwrite
-import SeisIO: KW, TimeSpec, check_for_gap!, dtconst, endtime, parsetimewin,
-  read_station_xml!, split_id, sxml_mergehdr!, trunc_x!
-import SeisIO.XMLwrite: mk_xml!
+using Dates, HDF5, SeisIO
+import SeisIO: KW, TimeSpec, check_for_gap!, dtconst, endtime, mk_xml!,
+  parsetimewin, read_station_xml!, split_id, sxml_mergehdr!, t_win, trunc_x!
+
+# These are adapted from unix2datetime.(1.0e-9.*[typemin(Int64), typemax(Int64)])
+const unset_s = "1677-09-21T00:12:44"
+const unset_t = "2262-04-11T23:47:16"
 
 # auxiliary functions
 include("SeisHDF/load_data.jl")
+include("SeisHDF/save_data.jl")
 include("SeisHDF/id_match.jl")
 include("SeisHDF/get_trace_bounds.jl")
+include("SeisHDF/asdf_aux.jl")
 
 # readers
 include("SeisHDF/read_asdf.jl")
@@ -21,10 +26,6 @@ include("SeisHDF/write_hdf5.jl")
 
 # scanner
 include("SeisHDF/scan_hdf5.jl")
-
-# These are adapted from unix2datetime.(1.0e-9.*[typemin(Int64), typemax(Int64)])
-const unset_s = "1677-09-21T00:12:44"
-const unset_t = "2262-04-11T23:47:16"
 
 export read_hdf5, read_hdf5!, scan_hdf5, write_hdf5
 
