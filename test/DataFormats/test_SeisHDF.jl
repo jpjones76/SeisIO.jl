e1d50374ca7e45340f29d5e3438afe32de85e68c
Joshua <jpjones76@users.noreply.github.com>
2019-10-23 02:25:47 -0700

    test improvements

--- test/DataFormats/test_SeisHDF.jl
+++ test/DataFormats/test_SeisHDF.jl
@@ -94,7 +94,15 @@ S3 = read_hdf5(hdf_out1, ts, te, id = id)
 for i in 1:S3.n
   S3.x[i] .= (2.0.*S3.x[i])
 end
-write_hdf5( hdf_out1, S3, ovr=true, v=3 )
+redirect_stdout(out) do
+  write_hdf5( hdf_out1, S3, ovr=true, v=3 )
+  push!(S3, SeisChannel(id="YY.ZZTOP.00.LEG", fs=50.0))
+  write_hdf5( hdf_out1, S3, v=3 )
+  @test_throws ErrorException write_hdf5( hdf_out1, S3, ovr=true, v=3 )
+  C = S3[1]
+  write_hdf5( hdf_out1, C, v=3 )
+  deleteat!(S3, 2)
+end
 
 # ASDF write test 3: write with gaps
 printstyled("      write to new file with gaps\n", color=:light_green)
@@ -114,90 +122,95 @@ scan3 = scan_hdf5(hdf_out1, level="trace")
 
 # ASDF write test 4: file with multiple stations and channels
 if has_restricted
-  S = read_data("sac", path*"/SampleFiles/Restricted/20140927000000*SAC")
-  write_hdf5( hdf_out1, S, v=3 )
+  redirect_stdout(out) do
+    S = read_data("sac", path*"/SampleFiles/Restricted/20140927000000*SAC")
+    write_hdf5( hdf_out1, S, v=3 )
 
-  id = S.id[3]
-  ts = "2014-09-27T09:00:00"
+    id = S.id[3]
+    ts = "2014-09-27T09:00:00"
 
-  # Test for intended read behavior
-  printstyled("        are written traces the right length?\n", color=:light_green)
-  te = "2014-09-27T10:00:00"
-  C = read_hdf5(hdf_out1, ts, te, id = id)[1]
-  @test length(C.x) == 360000 # Stop at last available sample
+    # Test for intended read behavior
+    printstyled("        are written traces the right length?\n", color=:light_green)
+    te = "2014-09-27T10:00:00"
+    C = read_hdf5(hdf_out1, ts, te, id = id)[1]
+    @test length(C.x) == 360000 # Stop at last available sample
 
-  te = "2014-09-27T09:59:59.99"
-  C = read_hdf5(hdf_out1, ts, te, id = id)[1]
-  @test length(C.x) == 360000 # Exact
+    te = "2014-09-27T09:59:59.99"
+    C = read_hdf5(hdf_out1, ts, te, id = id)[1]
+    @test length(C.x) == 360000 # Exact
 
-  ts = "2014-09-27T09:00:00.01"
-  te = "2014-09-27T09:59:59.90"
-  C2 = read_hdf5(hdf_out1, ts, te, id = id)[1]
-  @test length(C2.x) == 359990 # Exact, ten samples shorter
-  @test C.x[2:5] == C2.x[1:4]
+    ts = "2014-09-27T09:00:00.01"
+    te = "2014-09-27T09:59:59.90"
+    C2 = read_hdf5(hdf_out1, ts, te, id = id)[1]
+    @test length(C2.x) == 359990 # Exact, ten samples shorter
+    @test C.x[2:5] == C2.x[1:4]
 
-  # test 1: are the right trace names created?
-  printstyled("        are the right trace names created?\n", color=:light_green)
-  append!(S, S1)
-  safe_rm(hdf_out1)
-  write_hdf5(hdf_out1, S, add=true, ovr=true, v=3)
-  @test scan_hdf5(hdf_out1, level="trace") == [
-  "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-07T00:00:00__2019-07-07T23:59:59.975__hhz_",
-  "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-08T00:00:00__2019-07-08T23:59:59.975__hhz_",
-  "/Waveforms/CI.SDD/StationXML",
-  "/Waveforms/JP.VONTA/JP.VONTA..E__2014-09-27T00:00:00__2014-09-27T23:59:59.99__e_",
-  "/Waveforms/JP.VONTA/JP.VONTA..H__2014-09-27T00:00:00__2014-09-27T23:59:59.99__h_",
-  "/Waveforms/JP.VONTA/JP.VONTA..N__2014-09-27T00:00:00__2014-09-27T23:59:59.99__n_",
-  "/Waveforms/JP.VONTA/JP.VONTA..U__2014-09-27T00:00:00__2014-09-27T23:59:59.99__u_",
-  "/Waveforms/JP.VONTA/StationXML",
-  "/Waveforms/JP.VONTN/JP.VONTN..E__2014-09-27T00:00:00__2014-09-27T23:59:59.99__e_",
-  "/Waveforms/JP.VONTN/JP.VONTN..H__2014-09-27T00:00:00__2014-09-27T23:59:59.99__h_",
-  "/Waveforms/JP.VONTN/JP.VONTN..N__2014-09-27T00:00:00__2014-09-27T23:59:59.99__n_",
-  "/Waveforms/JP.VONTN/JP.VONTN..U__2014-09-27T00:00:00__2014-09-27T23:59:59.99__u_",
-  "/Waveforms/JP.VONTN/StationXML"
-  ]
+    # test 1: are the right trace names created?
+    printstyled("        are the right trace names created?\n", color=:light_green)
+    S1 = SeisData()
+    read_hdf5!(S1, hdf, "2019-07-07T23:00:00", "2019-07-08T02:00:00", id = "CI.SDD..HHZ")
 
-  # test 2: ONLY write CI.SDD..HHZ, JP.VONTA..H, JP.VONTA..N, JP.VONTA..U
-  printstyled("        write with a channel sublist\n", color=:light_green)
-  write_hdf5(hdf_out2, S, chans=[2,3,4,9], add=true, ovr=true, v=3)
-  @test scan_hdf5(hdf_out2, level="trace") == [
-  "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-07T00:00:00__2019-07-07T23:59:59.975__hhz_",
-  "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-08T00:00:00__2019-07-08T23:59:59.975__hhz_",
-  "/Waveforms/CI.SDD/StationXML",
-  "/Waveforms/JP.VONTA/JP.VONTA..H__2014-09-27T00:00:00__2014-09-27T23:59:59.99__h_",
-  "/Waveforms/JP.VONTA/JP.VONTA..N__2014-09-27T00:00:00__2014-09-27T23:59:59.99__n_",
-  "/Waveforms/JP.VONTA/JP.VONTA..U__2014-09-27T00:00:00__2014-09-27T23:59:59.99__u_",
-  "/Waveforms/JP.VONTA/StationXML"
-  ]
+    append!(S, S1)
+    safe_rm(hdf_out1)
+    write_hdf5(hdf_out1, S, add=true, ovr=true, v=3)
+    @test scan_hdf5(hdf_out1, level="trace") == [
+    "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-07T00:00:00__2019-07-07T23:59:59.975__hhz_",
+    "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-08T00:00:00__2019-07-08T23:59:59.975__hhz_",
+    "/Waveforms/CI.SDD/StationXML",
+    "/Waveforms/JP.VONTA/JP.VONTA..E__2014-09-27T00:00:00__2014-09-27T23:59:59.99__e_",
+    "/Waveforms/JP.VONTA/JP.VONTA..H__2014-09-27T00:00:00__2014-09-27T23:59:59.99__h_",
+    "/Waveforms/JP.VONTA/JP.VONTA..N__2014-09-27T00:00:00__2014-09-27T23:59:59.99__n_",
+    "/Waveforms/JP.VONTA/JP.VONTA..U__2014-09-27T00:00:00__2014-09-27T23:59:59.99__u_",
+    "/Waveforms/JP.VONTA/StationXML",
+    "/Waveforms/JP.VONTN/JP.VONTN..E__2014-09-27T00:00:00__2014-09-27T23:59:59.99__e_",
+    "/Waveforms/JP.VONTN/JP.VONTN..H__2014-09-27T00:00:00__2014-09-27T23:59:59.99__h_",
+    "/Waveforms/JP.VONTN/JP.VONTN..N__2014-09-27T00:00:00__2014-09-27T23:59:59.99__n_",
+    "/Waveforms/JP.VONTN/JP.VONTN..U__2014-09-27T00:00:00__2014-09-27T23:59:59.99__u_",
+    "/Waveforms/JP.VONTN/StationXML"
+    ]
 
-  # ensure the right trace data were written to these names
-  S1 = read_hdf5(hdf_out2, "2014-09-27T09:00:00", "2014-09-27T09:59:59.99", id="*.VONTA..*", msr=false)
-  S2 = S[[2,3,4]]
-  for f in (:id, :loc, :fs, :gain, :resp, :t, :x)
-    @test getfield(S1,f) == getfield(S2,f)
-  end
+    # test 2: ONLY write CI.SDD..HHZ, JP.VONTA..H, JP.VONTA..N, JP.VONTA..U
+    printstyled("        write with a channel sublist\n", color=:light_green)
+    write_hdf5(hdf_out2, S, chans=[2,3,4,9], add=true, ovr=true, v=3)
+    @test scan_hdf5(hdf_out2, level="trace") == [
+    "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-07T00:00:00__2019-07-07T23:59:59.975__hhz_",
+    "/Waveforms/CI.SDD/CI.SDD..HHZ__2019-07-08T00:00:00__2019-07-08T23:59:59.975__hhz_",
+    "/Waveforms/CI.SDD/StationXML",
+    "/Waveforms/JP.VONTA/JP.VONTA..H__2014-09-27T00:00:00__2014-09-27T23:59:59.99__h_",
+    "/Waveforms/JP.VONTA/JP.VONTA..N__2014-09-27T00:00:00__2014-09-27T23:59:59.99__n_",
+    "/Waveforms/JP.VONTA/JP.VONTA..U__2014-09-27T00:00:00__2014-09-27T23:59:59.99__u_",
+    "/Waveforms/JP.VONTA/StationXML"
+    ]
 
-  # ensure what's read back in are the correct traces with NaNs in the right places
-  printstyled("        trace indexing with ovr=true\n", color=:light_green)
-  S1 = read_hdf5(hdf_out2, "2014-09-27T08:00:00.00", "2014-09-27T10:00:00.00", id="*.VONTA..*")
-  for i in 1:S1.n
-    x = S1.x[i]
-    @test length(x) == 720001
-    @test isnan(last(x))
-    @test eltype(x) == Float32
-    for j in 1:360000
-      @test isnan(x[j])
+    # ensure the right trace data were written to these names
+    S1 = read_hdf5(hdf_out2, "2014-09-27T09:00:00", "2014-09-27T09:59:59.99", id="*.VONTA..*", msr=false)
+    S2 = S[[2,3,4]]
+    for f in (:id, :loc, :fs, :gain, :resp, :t, :x)
+      @test getfield(S1,f) == getfield(S2,f)
     end
+
+    # ensure what's read back in are the correct traces with NaNs in the right places
+    printstyled("        trace indexing with ovr=true\n", color=:light_green)
+    S1 = read_hdf5(hdf_out2, "2014-09-27T08:00:00.00", "2014-09-27T10:00:00.00", id="*.VONTA..*")
+    for i in 1:S1.n
+      x = S1.x[i]
+      @test length(x) == 720001
+      @test isnan(last(x))
+      @test eltype(x) == Float32
+      for j in 1:360000
+        @test isnan(x[j])
+      end
+    end
+
+    # only an hour of non-NaN data; 24 hours of data created
+    S1 = read_hdf5(hdf_out2, "2014-09-27T00:00:00.00", "2014-09-28T00:00:00.00", id="JP.VONTA..H")
+    @test length(S1.x[1]) == 8640000
+    @test length(S1.x[1])-length(findall(isnan.(S1.x[1]))) == 360000
+
+    # data from CI.SDD should have no NaNs in this range; it's the exact range of S.x[9]
+    S1 = read_hdf5(hdf_out2, "2019-07-07T23:00:00", "2019-07-08T02:00:00")
+    @test S1.x[1] == S.x[9]
   end
-
-  # only an hour of non-NaN data; 24 hours of data created
-  S1 = read_hdf5(hdf_out2, "2014-09-27T00:00:00.00", "2014-09-28T00:00:00.00", id="JP.VONTA..H")
-  @test length(S1.x[1]) == 8640000
-  @test length(S1.x[1])-length(findall(isnan.(S1.x[1]))) == 360000
-
-  # data from CI.SDD should have no NaNs in this range; it's the exact range of S.x[9]
-  S1 = read_hdf5(hdf_out2, "2019-07-07T23:00:00", "2019-07-08T02:00:00")
-  @test S1.x[1] == S.x[9]
 end
 
 # HDF write cleanup
