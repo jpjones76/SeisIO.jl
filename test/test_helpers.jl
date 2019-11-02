24e4e3e3a7924b6c2c95a40a52d39ce27e94e251
Joshua Jones <jpjones76@users.noreply.github.com>
2019-11-02 02:15:08 -0700

    add compare_events

--- test/test_helpers.jl
+++ test/test_helpers.jl
@@ -322,6 +322,73 @@ function safe_rm(file::String)
   return nothing
 end
 
+function compare_events(Ev1::SeisEvent, Ev2::SeisEvent)
+  H1 = Ev1.hdr
+  H2 = Ev2.hdr
+  for f in fieldnames(EQLoc)
+    if typeof(getfield(H1.loc, f)) <: AbstractFloat
+      @test isapprox(getfield(H1.loc, f), getfield(H2.loc,f))
+    else
+      @test getfield(H1.loc, f) == getfield(H2.loc,f)
+    end
+  end
+  @test H1.mag.val ≈ H2.mag.val
+  @test H1.mag.gap ≈ H2.mag.gap
+  @test H1.mag.src == H2.mag.src
+  @test H1.mag.scale == H2.mag.scale
+  @test H1.mag.nst == H2.mag.nst
+  @test H1.id == H2.id
+  @test H1.ot == H2.ot
+  @test H1.src == H2.src
+  @test H1.typ == H2.typ
+
+  R1 = Ev1.source
+  R2 = Ev2.source
+  @test R1.id == R2.id
+  @test R1.eid == R2.eid
+  @test R1.m0 ≈ R2.m0
+  @test R1.mt ≈ R2.mt
+  @test R1.dm ≈ R2.dm
+  @test R1.gap ≈ R2.gap
+  @test R1.pax ≈ R2.pax
+  @test R1.planes ≈ R2.planes
+  @test R1.src == R2.src
+  @test R1.st.desc == R2.st.desc
+  @test R1.st.dur ≈ R2.st.dur
+  @test R1.st.rise ≈ R2.st.rise
+  @test R1.st.decay ≈ R2.st.decay
+
+  S1 = convert(SeisData, Ev1.data)
+  S2 = convert(SeisData, Ev2.data)
+  sort!(S1)
+  sort!(S2)
+  @test S1.id == S2.id
+  @test S1.name == S2.name
+  @test S1.units == S2.units
+  @test isapprox(S1.fs, S2.fs)
+  @test isapprox(S1.gain, S2.gain)
+  for i in 1:S1.n
+    L1 = S1.loc[i]
+    L2 = S2.loc[i]
+    @test isapprox(L1.lat, L2.lat)
+    @test isapprox(L1.lon, L2.lon)
+    @test isapprox(L1.el, L2.el)
+    @test isapprox(L1.dep, L2.dep)
+    @test isapprox(L1.az, L2.az)
+    @test isapprox(L1.inc, L2.inc)
+
+    R1 = S1.resp[i]
+    R2 = S2.resp[i]
+    for f in fieldnames(PZResp)
+      @test isapprox(getfield(R1, f), getfield(R2,f))
+    end
+
+    @test S1.t[i] == S2.t[i]
+    @test isapprox(S1.x[i],S2.x[i])
+  end
+  return nothing
+end
+
 # ===========================================================================
 # Redirect info, warnings, and errors to the logger
 out = open("runtests.log", "a")
