5dc0bcbdd8bc3999a46358ca1ae007d7ee70a434
Joshua Jones <jpjones76@users.noreply.github.com>
2019-11-20 22:38:36 -0800

    added test_get_svn.jl

---
+++ test/CoreUtils/test_get_svn.jl
@@ -0,0 +1,12 @@
+right_url = "https://github.com/jpjones76/SeisIO-TestData/trunk/SVN_test"
+wrong_url = "https://github.com/jpjones76/SeisIO-TestData/trunk/DOESNT_EXIST"
+svn_dir = "SVN_test"
+svn_file = joinpath(svn_dir, "test")
+
+get_svn(right_url, svn_dir)
+@test isfile(svn_file)
+str = readline(svn_file)
+@test str == "."
+rm(svn_file)
+rm(svn_dir)
+@test_throws ErrorException get_svn(wrong_url, "SVN_test")
