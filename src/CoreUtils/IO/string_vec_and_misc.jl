93817890b52a0e759e32161cd4815110cba076ed
Joshua <jpjones76@users.noreply.github.com>
2020-02-13 16:56:28 -0800

    renamed file_io.jl

--- src/CoreUtils/IO/string_vec_and_misc.jl
+++ src/CoreUtils/IO/string_vec_and_misc.jl
@@ -1,49 +1,49 @@
-function read_string_vec(io::IO, u::Array{UInt8,1})
-  b = read(io, UInt8)
+function read_string_vec(io::IOStream, u::Array{UInt8,1})
+  b = fastread(io)
   S = String[]
   if b == 0x00
-    N = read(io, Int64)
+    N = fastread(io, UInt64)
     p = pointer(u)
-    L = read!(io, Array{UInt16,1}(undef, N))
+    L = fastread(io, UInt16, N)
     i = 0
     while i < N
       i = i + 1
       l = getindex(L, i)
-      unsafe_read(io, p, l)
+      fast_unsafe_read(io, p, l)
       push!(S, unsafe_string(p, l))
     end
   end
   return S
 end
 
-function read_misc(io::IO, u::Array{UInt8,1})
+function read_misc(io::IOStream, u::Array{UInt8,1})
   D = Dict{String,Any}()
-  L = read(io, Int64)
+  L = fastread(io, UInt64)
   if L != zero(Int64)
     n     = zero(Int64)
     dims  = Array{Int64, 1}(undef, 3)
     p     = pointer(u)
     K     = read_string_vec(io, u)
     for k in K
-      t = read(io, UInt8)
+      t = fastread(io)
       T = code2typ(t)
 
       # String array
       if t == 0x81
-        nd = read(io, Int64)
+        nd = fastread(io, Int64)
         checkbuf_strict!(dims, nd)
-        read!(io, dims)
+        fastread!(io, dims)
         if dims == [0]
           D[k] = String[]
         else
           S = String[]
           i = 0
           N = prod(dims)
-          L = read!(io, Array{UInt16,1}(undef, N))
+          L = fastread(io, UInt16, N)
           while i < N
             i = i + 1
             l = getindex(L, i)
-            unsafe_read(io, p, l)
+            fast_unsafe_read(io, p, l)
             push!(S, unsafe_string(p, l))
           end
           D[k] = reshape(S, dims...)
@@ -51,22 +51,25 @@ function read_misc(io::IO, u::Array{UInt8,1})
 
       # Numeric or Char array
       elseif T <: AbstractArray
-        nd = read(io, Int64)
+        nd = fastread(io, Int64)
         if nd == 1
-          D[k] = read!(io, T(undef, read(io, Int64)))
+          D[k] = read!(io, T(undef, fastread(io, Int64)))
         else
           checkbuf_strict!(dims, nd)
-          read!(io, dims)
+          fastread!(io, dims)
           D[k] = read!(io, T(undef, dims...))
         end
 
       # String
       elseif T == String
-        n = read(io, UInt16)
-        unsafe_read(io, p, n)
+        n = fastread(io, UInt16)
+        fast_unsafe_read(io, p, n)
         D[k] = unsafe_string(p, n)
 
       # Bits type
+      elseif Type{T} <: FastReads
+        D[k] = fastread(io, T)
+
       else
         D[k] = read(io, T)
       end
