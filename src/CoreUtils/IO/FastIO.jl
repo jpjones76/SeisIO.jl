7b3e28f47d491a4011282d86dfdb9946b5285cd8
Joshua <jpjones76@users.noreply.github.com>
2020-02-14 01:31:06 -0800

    update FastIO.jl

--- src/CoreUtils/IO/FastIO.jl
+++ src/CoreUtils/IO/FastIO.jl
@@ -15,35 +15,29 @@ IBM-Float
 =#
 
 # fastpos
-fastpos(io::Base.GenericIOBuffer) = io.ptr-1
+fastpos(io::IO) = io.ptr-1
 fastpos(io::IOStream) = ccall(:ios_pos, Int64, (Ptr{Cvoid},), io.ios)
 
 # fasteof
-fasteof(io::Base.GenericIOBuffer) = (io.ptr-1 == io.size)
+fasteof(io::IO) = (io.ptr-1 == io.size)
 fasteof(io::IOStream) = Bool(ccall(:ios_eof_blocking, Cint, (Ptr{Cvoid},), io.ios))
 
 # fastseek
-function fastseek(io::Base.GenericIOBuffer, p::Integer)
-  p -= 1
-  (p < 0) && throw(ArgumentError("Attempted to seek outside IOBuffer boundaries."))
-  (p-1 > io.size) && throw(ArgumentError("Attempted to seek outside IOBuffer boundaries."))
-  io.ptr = p
-  return nothing
-end
+fastseek(io::IO, p::Integer) = seek(io, p)
 function fastseek(io::IOStream, n::Integer)
   ccall(:ios_seek, Int64, (Ptr{Cvoid}, Int64), io.ios, n)
   return nothing
 end
 
 # fastskip
-fastskip(io::Base.GenericIOBuffer, n::Integer) = skip(io, n)
+fastskip(io::IO, n::Integer) = skip(io, n)
 function fastskip(io::IOStream, n::Integer)
   ccall(:ios_skip, Int64, (Ptr{Cvoid}, Int64), io.ios, n)
   return nothing
 end
 
 # fastseekend
-fastseekend(io::Base.GenericIOBuffer) = (io.ptr = io.size+1)
+fastseekend(io::IO) = (io.ptr = io.size+1)
 fastseekend(io::IOStream) = ccall(:ios_seek_end, Int64, (Ptr{Cvoid},), io.ios)
 
 # =====================================================================
@@ -51,10 +45,6 @@ fastseekend(io::IOStream) = ccall(:ios_seek_end, Int64, (Ptr{Cvoid},), io.ios)
 
 fastread(io) returns a UInt8
 fastread(io, T) returns a T
-fastread_i32(io::IO, N::Integer; b::Bool=false) reads an N-byte Integer to Int32.
-fastread_iN(io): read N bits to Int32 (for N ≤ 32) or Int64; defined for N = 8, 16, 24
-fastread_fN(io): return 32-bit float converted from an n-bit float. Defined for N = 16, 64.
-fastread_i64(io::IO, N::Integer; b::Bool=false) reads an N-byte Integer to Int64.
 
 =#
 fastread!(io::IO, buf::Array{T}) where T = read!(io, buf)
@@ -62,10 +52,9 @@ fastread!(io::IOStream, buf::Array{T}) where T = @GC.preserve buf ccall(:ios_rea
 # fastread!(io::IOStream, buf::Array{UInt8,1}) = ccall(:ios_readall, Csize_t, (Ptr{Cvoid}, Ptr{Cvoid}, Csize_t), io.ios, pointer(buf, 1), sizeof(buf))
 
 # fastread
-fastread(io::Base.GenericIOBuffer) = read(io, UInt8)
-fastread(io::Base.GenericIOBuffer, T::FastReadInt) = read(io, T)
-fastread(io::Base.GenericIOBuffer, T::Union{Type{Float32}, Type{Float64}}) = read(io, T)
-fastread(io::Base.GenericIOBuffer, n::Integer) = read(io, n)
+fastread(io::IO) = read(io, UInt8)
+fastread(io::IO, T::FastReadInt) = read(io, T)
+fastread(io::IO, n::Integer) = read(io, n)
 
 # IOStream methods avoid locking in 1.3
 fastread(io::IOStream) = ccall(:ios_getc, Cint, (Ptr{Cvoid},), io.ios) % UInt8
@@ -92,67 +81,12 @@ end
 #   return signed(x % UInt16)
 # end
 #
-fastread(io::IO, ::Type{Bool})    = fastread(io) % Bool
+fastread(io::IO, ::Type{Bool})    = (fastread(io) != 0x00)
 fastread(io::IO, ::Type{UInt8})   = fastread(io)
 fastread(io::IO, ::Type{Int8})    = signed(fastread(io))
 fastread(io::IO, ::Type{Float16}) = Base.reinterpret(Float16, fastread(io, Int16))
 fastread(io::IO, ::Type{Float32}) = Base.reinterpret(Float32, fastread(io, Int32))
 fastread(io::IO, ::Type{Float64}) = Base.reinterpret(Float64, fastread(io, Int64))
-
-"""
-    fastread_i32(io, n::Integer; b::Bool=false, s::Bool=true)
-
-Read an `n`-byte integer from `io` into an Int32 (s=true) or UInt32 (s=false). Specify `b=true` for big-Endian byte order.
-
-Useful for things like Int24. Maybe faster than bswap(fastread(io, Int32)); in testing.
-
-    fastread_i64(io, n::Integer; b::Bool=false, s::Bool=true)
-
-Read an `n`-byte integer from `io` into an Int64 (s=true) or UInt64 (s=false). Specify `b=true` for big-Endian byte order.
-"""
-function fastread_i32(io::IO, n::Integer; b::Bool=false)
-  x = UInt32(fastread(io))
-  if b
-    N = 0x08*(UInt8(n)-0x01)
-    x <<= N
-    for i in 2:n
-      N -= 0x08
-      x |= UInt32(fastread(io)) << N
-    end
-  else
-    N = 0x08
-    for i in 2:n
-      x |= UInt32(fastread(io)) << N
-      N += 0x08
-    end
-  end
-  return signed(x)
-end
-fastread_i8(io::IO) = fastread(io) % Int32
-fastread_i16(io::IO; b::Bool=false) = fastread_i32(io, 2, b=b)
-fastread_i24(io::IO; b::Bool=false) = fastread_i32(io, 3, b=b)
-fastread_i32(io::IO; b::Bool=false) = fastread_i32(io, 4, b=b)
-fastread_f16(io::IO; b::Bool=false) = Float32(fastread(io, Float16))
-fastread_f32(io::IO; b::Bool=false) = fastread(io, Float32)
-fastread_f64(io::IOStream; b::Bool=false) = Float32(fastread(io, Float64))
-function fastread_i64(io::IOStream, n::Integer; b::Bool=false, s::Bool=true)
-  x = UInt64(fastread(io))
-  if b
-    N = 0x08*(UInt8(n)-0x01)
-    x <<= N
-    for i in 2:n
-      N -= 0x08
-      x |= UInt64(fastread(io)) << N
-    end
-  else
-    N = 0x08
-    for i in 2:n
-      x |= UInt64(fastread(io)) << N
-      N += 0x08
-    end
-  end
-  return s ? signed(x) : x
-end
 
 # use if creating a new array during the call; I have yet to read anything but a 1d array in any format
 function fastread(io::IO, T::Type, n::Integer)
@@ -196,22 +130,6 @@ fast_unsafe_read(io::IO, p::Ptr{UInt8}, nb::Integer) = unsafe_read(io, p, nb)
 fast_unsafe_read(io::IOStream, p::Ptr{UInt8}, nb::Integer) = ccall(:ios_readall, Csize_t, (Ptr{Cvoid}, Ptr{Cvoid}, Csize_t), io, p, nb)
 
 # =====================================================================
-# fastwrite
-
-# literally just the Julia functions without the lock macros
-# fastwrite(io::IO, b::UInt8) = write(io, b)
-# function fastwrite(io::IOStream, b::UInt8)
-#     iswritable(io) || throw(ArgumentError("write failed, IOStream is not writeable"))
-#     ccall(:ios_putc, Cint, (Cint, Ptr{Cvoid}), b, io.ios)
-# end
-#
-# fast_unsafe_write(io::IO, p::Ptr{UInt8}, nb::Integer) = unsafe_write(io, p, nb)
-# function fast_unsafe_write(io::IOStream, p::Ptr{UInt8}, nb::Integer)
-#     iswritable(s) || throw(ArgumentError("write failed, IOStream is not writeable"))
-#     ccall(:ios_write, Csize_t, (Ptr{Cvoid}, Ptr{Cvoid}, Csize_t), io.ios, p, nb)
-# end
-
-# =====================================================================
 export
   FastReadInt,
   FastReads,
@@ -222,14 +140,6 @@ export
   fastpos,
   fastread!,
   fastread,
-  fastread_f16,
-  fastread_f32,
-  fastread_f64,
-  fastread_i8,
-  fastread_i16,
-  fastread_i24,
-  fastread_i32,
-  fastread_i64,
   fastseek,
   fastseekend,
   fastskip
