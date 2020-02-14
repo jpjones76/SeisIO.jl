29a619cd701733435eb8cf84534c5dab77bcadb1
Joshua <jpjones76@users.noreply.github.com>
2020-02-13 17:15:31 -0800

    moved imports, exports to own files

---
+++ src/Submodules/RandSeis/exports.jl
@@ -0,0 +1,7 @@
+export randSeisChannel,
+  randSeisData,
+  randSeisEvent,
+  randSeisHdr,
+  randPhaseCat,
+  randSeisSrc
+  
---
+++ src/Submodules/RandSeis/imports.jl
@@ -0,0 +1,8 @@
+import Dates: now
+import Random: randexp, randstring, shuffle!
+import SeisIO: InstrumentPosition,
+  code2typ,
+  getbandcode,
+  note!,
+  sμ,
+  μs
