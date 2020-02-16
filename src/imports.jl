72dbbbfec55d6b769f45a7dc4e6a7a5812fd7d66
Joshua <jpjones76@users.noreply.github.com>
2020-02-16 14:23:34 -0800

    update imports.jl

--- src/imports.jl
+++ src/imports.jl
@@ -1,6 +1,5 @@
 import Base: ==, -, *, +, append!, convert, copy, delete!, deleteat!,
-  firstindex, getindex, hash, in, isempty, isequal, lastindex, length, 
+  firstindex, getindex, hash, in, isempty, isequal, lastindex, length,
   push!, merge, merge!, read, setindex!, show, size, sizeof, sort!,
   sort, summary, write
-import Base.Libc: TmStruct
 import DSP: filtfilt
