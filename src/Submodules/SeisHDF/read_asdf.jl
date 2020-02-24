c41d1473e506a05456e6ea591a70a049304a5b30
Joshua <jpjones76@users.noreply.github.com>
2020-02-24 02:10:09 -0800

    renamed variables to be less confusing

--- src/Submodules/SeisHDF/read_asdf.jl
+++ src/Submodules/SeisHDF/read_asdf.jl
@@ -1,10 +1,51 @@
+#= List of Variables
+
+tr      trace dataset
+wv      waveform dataset
+sta     station dataset
+idₛ     station id
+idₜ     trace id
+stas    list of station names
+traces  list of trace names
+trₛ     names of traces in sta
+
+idⱼ     S[j] :id
+iⱼ      S[j] first index in :x to write
+jⱼ      S[j] last index in :x to write
+lxⱼ     S[j] value of :t[end,1]
+ntⱼ     S[j] number of times in :t[:,1]
+nⱼ      S[j] length(:x)
+tⱼ      S[j] :t
+
+eᵣ      request window end time [ns from epoch]
+nᵣ      request window length in samples
+sᵣ      request window start time [ns from epoch]
+
+eₜ      trace end time [ns from epoch]
+fs      trace sampling frequency [Hz]
+iₜ      trace first index to read
+jₜ      trace last index to read
+nₜ      trace total number of samples
+sₜ      trace start time [ns from epoch]
+
+nₒ      overlap window number of samples
+sₒ      overlap window start time [ns from epoch]
+
+j         index to channel in S
+v         verbosity
+Δ         sampling interval [ns]
+
+because I managed to confuse even myself with the bookkeeping, and that's not
+happened since coding SeisIO began
+=#
+
 function read_asdf!(  S::GphysData,
                       hdf::String,
                       id::String,
                       s::TimeSpec,
                       t::TimeSpec,
                       msr::Bool,
-                      v::Int64  )
+                      v::Integer  )
 
   SX = SeisData() # for XML
   idr = isa(id, String) ? id_to_regex(id) : id
@@ -13,15 +54,15 @@ function read_asdf!(  S::GphysData,
   if typeof(s) == String && typeof(t) == String
     d0 = s
     d1 = t
-    ts = DateTime(s).instant.periods.value*1000 - dtconst
-    te = DateTime(t).instant.periods.value*1000 - dtconst
+    sᵣ = DateTime(s).instant.periods.value*1000 - dtconst
+    eᵣ = DateTime(t).instant.periods.value*1000 - dtconst
   else
     (d0, d1) = parsetimewin(s, t)
-    ts = DateTime(d0).instant.periods.value*1000 - dtconst
-    te = DateTime(d1).instant.periods.value*1000 - dtconst
+    sᵣ = DateTime(d0).instant.periods.value*1000 - dtconst
+    eᵣ = DateTime(d1).instant.periods.value*1000 - dtconst
   end
-  ts *= 1000
-  te *= 1000
+  sᵣ *= 1000
+  eᵣ *= 1000
   Δ = 0
   fs = 0.0
 
@@ -29,74 +70,73 @@ function read_asdf!(  S::GphysData,
   netsta = netsta_to_regex(id)
   idr = id_to_regex(id)
   f = h5open(hdf, "r")
-  W = f["Waveforms"]
-  A = names(W)
-  sort!(A)
-  (v > 2) && println("Net.sta found: ", A)
+  wv = f["Waveforms"]
+  stas = names(wv)
+  sort!(stas)
+  (v > 2) && println("Net.sta found: ", stas)
 
-  for i in A
-    if occursin(netsta, i)
-      w = W[i]
-      N = names(w)
-      sort!(N)
-      (v > 2) && println("Traces found: ", N)
-      for n in N
-        if n == "StationXML"
-          sxml = String(UInt8.(read(w[n])))
+  for idₛ in stas
+    if occursin(netsta, idₛ)
+      sta = wv[idₛ]
+      traces = names(sta)
+      sort!(traces)
+      (v > 2) && println("Traces found: ", traces)
+      for idₜ in traces
+        if idₜ == "StationXML"
+          sxml = String(UInt8.(read(sta[idₜ])))
           read_station_xml!(SX, sxml, msr=msr, s=s, t=t, v=v)
-        elseif occursin(idr, n)
-          x = w[n]
-          nx = length(x)
-          t0 = read(x["starttime"])
-          fs = read(x["sampling_rate"])
+        elseif occursin(idr, idₜ)
+          tr = sta[idₜ]
+          nₜ = length(tr)
+          sₜ = read(tr["starttime"])
+          fs = read(tr["sampling_rate"])
 
           # convert fs to sampling interval in ns
           Δ = round(Int64, 1.0e9/fs)
-          t1 = t0 + (nx-1)*Δ
-          (v > 2) && println("t0 = ", t0,"; t1 = ", t1, "; ts = ", ts, "; te = ", te)
+          eₜ = sₜ + (nₜ-1)*Δ
+          (v > 2) && println("sₜ = ", sₜ,"; eₜ = ", eₜ, "; sᵣ = ", sᵣ, "; eᵣ = ", eᵣ)
 
-          if (ts ≤ t1) && (te ≥ t0)
-            xi = 0
-            i0, i1, t2 = get_trace_bounds(ts, te, t0, t1, Δ, nx)
-            ni = i1-i0+1
-            cid = String(split(n, "_", limit=2, keepempty=true)[1])
-            j = findid(cid, S.id)
-            (v > 2) && println("cid = ", cid, " (found at index in S = ", j, ")")
-            nX = div(te-ts, Δ)+1
+          if (sᵣ ≤ eₜ) && (eᵣ ≥ sₜ)
+            lxⱼ = 0
+            iₜ, jₜ, sₒ = get_trace_bounds(sᵣ, eᵣ, sₜ, eₜ, Δ, nₜ)
+            nₒ = jₜ-iₜ+1
+            idⱼ = String(split(idₜ, "_", limit=2, keepempty=true)[1])
+            j = findid(idⱼ, S.id)
+            (v > 2) && println("idⱼ = ", idⱼ, " (found at index in S = ", j, ")")
+            nᵣ = div(eᵣ-sᵣ, Δ)+1
             if j == 0
-              T = eltype(x)
-              push!(S, SeisChannel(id = cid,
+              T = eltype(tr)
+              push!(S, SeisChannel(id = idⱼ,
                                    fs = fs,
-                                   x = Array{T,1}(undef, nX)))
+                                   x = Array{T,1}(undef, nᵣ)))
               j = S.n
-              if has(x, "event_id")
-                S.misc[j]["event_id"] = read(x["event_id"])
+              if has(tr, "event_id")
+                S.misc[j]["event_id"] = read(tr["event_id"])
               end
             else
-              ct = getindex(getfield(S, :t), j)
-              nt = div(lastindex(ct), 2)
-              L = lastindex(S.x[j])
-              if nt > 0
-                xi = getindex(ct, nt)
-                check_for_gap!(S, j, div(t2, 1000), ni, v)
+              tⱼ = getindex(getfield(S, :t), j)
+              ntⱼ = div(lastindex(tⱼ), 2)
+              nⱼ = lastindex(S.x[j])
+              if ntⱼ > 0
+                lxⱼ = getindex(tⱼ, ntⱼ)
+                check_for_gap!(S, j, div(sₒ, 1000), nₒ, v)
               end
-              if xi + ni > L
-                resize!(S.x[j], xi + max(ni, nX))
+              if lxⱼ + nₒ > nⱼ
+                resize!(S.x[j], lxⱼ + max(nₒ, nᵣ))
               end
               if S.fs[j] == 0.0
                 S.fs[j] = fs
               end
             end
 
-            if xi == 0
-              ct = mk_t(ni, div(t2, 1000))
-              setindex!(getfield(S, :t), ct, j)
+            if lxⱼ == 0
+              tⱼ = mk_t(nₒ, div(sₒ, 1000))
+              setindex!(getfield(S, :t), tⱼ, j)
             end
 
-            si = xi+1
-            ei = si+ni-1
-            X = S.x[j]
-            load_data!(X, x, i0:i1, si:ei)
+            iⱼ = lxⱼ+1
+            jⱼ = iⱼ+nₒ-1
+            load_data!(S.x[j], tr, iₜ:jₜ, iⱼ:jⱼ)
           end
         end
       end
@@ -116,7 +156,7 @@ function read_asdf!(  S::GphysData,
   return S
 end
 
-function read_asdf(hdf::String, id::Union{String,Regex}, s::TimeSpec, t::TimeSpec, msr::Bool, v::Int64)
+function read_asdf(hdf::String, id::Union{String,Regex}, s::TimeSpec, t::TimeSpec, msr::Bool, v::Integer)
   S = SeisData()
   read_asdf!(S, hdf, id, s, t, msr, v)
   return S
