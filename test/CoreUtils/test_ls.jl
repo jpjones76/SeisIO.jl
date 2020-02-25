605d4af17134a9d478b1dcd6bf421d8a75b7b723
Joshua <jpjones76@users.noreply.github.com>
2020-02-24 16:48:11 -0800

    adjust for removal of duplicate test file

--- test/CoreUtils/test_ls.jl
+++ test/CoreUtils/test_ls.jl
@@ -61,7 +61,7 @@ if safe_isfile(cfile) && arch_allowed
                 "/SampleFiles/*",
                 "/SampleFiles/Restricted/2014092709*cnt"
               ]
-  T_expect =  [63, 538, 60]
+  T_expect =  [63, 537, 60]
   if safe_isfile(path .* "/SampleFiles/restricted.tar.gz")
     T_expect[2] += 1
   end
