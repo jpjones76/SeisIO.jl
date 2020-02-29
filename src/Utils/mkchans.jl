c39690b1c89e4a8ad60ee90852db8e7a35280aa7
Joshua <jpjones76@users.noreply.github.com>
2020-02-29 00:41:28 -0800

    remove commented-out method

--- src/Utils/mkchans.jl
+++ src/Utils/mkchans.jl
@@ -1,18 +1,5 @@
 export mkchans
 
-# function mkchans(chans::Union{Integer, UnitRange, Array{Int64,1}}, n::Int64)
-#
-#   if chans == Int64[]
-#     return Int64.(collect(1:n))
-#   elseif typeof(chans) <: UnitRange
-#     return Int64.(collect(chans))
-#   elseif typeof(chans) <: Integer
-#     return [Int64(chans)]
-#   else
-#     return chans
-#   end
-# end
-
 function mkchans(chans::Union{Integer, UnitRange, Array{Int64,1}}, S::GphysData; f::Symbol=:x, keepempty::Bool=false)
   chan_list = (if chans == Int64[]
     Int64.(collect(1:S.n))
