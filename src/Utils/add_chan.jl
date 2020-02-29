a6c309bd2d88f78d180d8465ee7e00dadecb7531
Joshua <jpjones76@users.noreply.github.com>
2020-02-29 10:37:10 -0800

    add shortcut for empty S

--- src/Utils/add_chan.jl
+++ src/Utils/add_chan.jl
@@ -1,5 +1,9 @@
 function add_chan!(S::GphysData, C::GphysChannel, strict::Bool)
-  j = strict ? findid(S, C.id) : channel_match(S, C)
+  if isempty(S)
+    push!(S, C)
+    return 1
+  end
+  j = strict ? channel_match(S, C) : findid(S, C.id)
   if j > 0
     S.t[j] = t_extend(S.t[j], C.t[1,2], length(C.x), C.fs)
     append!(S.x[j], C.x)
