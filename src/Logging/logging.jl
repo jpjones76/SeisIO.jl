d4b01e84f8e8b7d3a3509f5421ac0d9adcc4473b
Joshua <jpjones76@users.noreply.github.com>
2020-03-02 02:54:07 -0800

    remove mystery function that I never implemented

--- src/Logging/logging.jl
+++ src/Logging/logging.jl
@@ -6,17 +6,19 @@ function fread_note!(S::GphysData, N::Array{Int64,1}, method::String, fmt::Strin
   return nothing
 end
 
-function mread_note!(S::GphysData, N::Array{Int64,1}, method::String, fmt::String, files::Array{String, 1}, opts::String)
-  for fname in files
-    note!(S, N, string( "+meta ¦ read_meta!(S, ", fmt, ", ", fname, ", ",
-                          "msr=", msr,  ", ",
-                          "s=\"", s,  "\", ",
-                          "t=\"", t,  "\", ",
-                          "units=", units, ", ",
-                          "v=", KW.v, ")" ) )
-  end
-  return nothing
-end
+# what is this?
+#
+# function mread_note!(S::GphysData, N::Array{Int64,1}, method::String, fmt::String, files::Array{String, 1}, opts::String)
+#   for fname in files
+#     note!(S, N, string( "+meta ¦ read_meta!(S, ", fmt, ", ", fname, ", ",
+#                           "msr=", msr,  ", ",
+#                           "s=\"", s,  "\", ",
+#                           "t=\"", t,  "\", ",
+#                           "units=", units, ", ",
+#                           "v=", KW.v, ")" ) )
+#   end
+#   return nothing
+# end
 
 # Five arguments: S, N, method, fname, opts
 function fwrite_note!(S::GphysData, i::Int64, method::String, fname::String, opts::String)
