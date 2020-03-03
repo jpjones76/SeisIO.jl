7ec6bda867d4383880803e6b262a4c4b740a505e
Joshua <jpjones76@users.noreply.github.com>
2020-03-02 21:40:55 -0800

    minor string change

--- src/Submodules/Quake/Utils/logging.jl
+++ src/Submodules/Quake/Utils/logging.jl
@@ -1,5 +1,5 @@
 function fwrite_note_quake!(C::Union{SeisHdr, SeisSrc}, method::String, fname::String, opts::String)
-  wstr = string(timestamp(), " ¦ write ¦ ", method, "(C", opts, ") ¦ wrote to file ", fname)
+  wstr = string(timestamp(), " ¦ write ¦ ", method, "(H", opts, ") ¦ wrote to file ", fname)
   push!(C.notes, wstr)
   return nothing
 end
