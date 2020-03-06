43dcfaa3f3ac340c914f6e812cccc37dbf7c8046
Joshua <jpjones76@users.noreply.github.com>
2020-03-05 20:27:05 -0800

    print filename in output of show_writes

--- src/Logging/printing.jl
+++ src/Logging/printing.jl
@@ -12,7 +12,7 @@ function print_log(notes::Array{String,1}, k::String)
     if nn[2] == k
       (ee == true) && (ee = false)
       func_str = (L > mm) ? (nn[3][firstindex(nn[3]):prevind(nn[3], mm)] * "…") : nn[3]
-      if k == "processing"
+      if k in ("processing", "write")
         pl *= string("| ", nn[1], "|`", func_str, "`|", nn[4], "|\n")
       else
         pl *= string("| ", nn[1], "|`", func_str, "`|\n")
@@ -35,7 +35,7 @@ end
 
 Tabulate and print all processing steps in `:notes` to stdout in human-readable format.
 
-See also: `show_src`, `note!`, `clear_notes!`
+See also: `show_src`, `show_writes`, `note!`, `clear_notes!`
 """
 function show_processing(S::GphysData)
   for i in 1:S.n
@@ -54,7 +54,7 @@ show_processing(C::GphysChannel) = print_log(C.notes, "processing")
 
 Tabulate and print all data sources logged in `:notes` to stdout in human-readable format.
 
-See also: `show_processing`, `note!`, `clear_notes!`
+See also: `show_processing`, `show_writes`, `note!`, `clear_notes!`
 """
 function show_src(S::GphysData)
   for i in 1:S.n
@@ -73,7 +73,7 @@ show_src(C::GphysChannel) = print_log(C.notes, "+source")
 
 Tabulate and print all data writes logged in `:notes` to stdout in human-readable format.
 
-See also: `show_processing`, `note!`, `clear_notes!`
+See also: `show_processing`, `show_src`, `note!`, `clear_notes!`
 """
 function show_writes(S::GphysData)
   for i in 1:S.n
