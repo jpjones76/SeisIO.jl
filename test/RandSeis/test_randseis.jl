5f6c7fde000c678f67e13f599852b71c4f6a2e4e
Joshua <jpjones76@users.noreply.github.com>
2020-03-07 21:41:10 -0800

    update test_randseis.jl

--- test/RandSeis/test_randseis.jl
+++ test/RandSeis/test_randseis.jl
@@ -1,14 +1,16 @@
-printstyled("  RandSeis utils\n", color=:light_green)
+printstyled("RandSeis\n", color=:light_green)
 
 fs_range = exp10.(range(-6, stop=4, length=50))
 fc_range = exp10.(range(-4, stop=2, length=20))
 
+printstyled("  getbandcode\n", color=:light_green)
 for fs in fs_range
   for fc in fc_range
     getbandcode(fs, fc=fc)
   end
 end
 
+printstyled("  pop_rand_dict!\n", color=:light_green)
 # This should do it
 D = Dict{String,Any}()
 for i = 1:10
@@ -16,32 +18,58 @@ for i = 1:10
 end
 
 # Similarly for the yp2 codes
-for i = 1:100
+printstyled("  getyp2codes\n", color=:light_green)
+for i = 1:1000
   i,c,u = RandSeis.getyp2codes('s', true)
   @test isa(i, Char)
   @test isa(c, Char)
   @test isa(u, String)
-end
 
-R = RandSeis.randResp(8)
-@test length(R.z) == length(R.p) == 8
-
-for i = 1:10000
   i,c,u = RandSeis.getyp2codes('s', false)
   @test isa(i, Char)
   @test isa(c, Char)
   @test isa(u, String)
 end
 
+# check that rand_t produces sane gaps
+printstyled("  rand_t\n", color=:light_green)
+fs = 100.0
+nx = 100
+
+printstyled("    controlled n_gaps\n", color=:light_green)
+t = RandSeis.rand_t(fs, nx, 10)
+@test size(t, 1) == 12
+t = RandSeis.rand_t(fs, nx, 0)
+@test size(t, 1) == 2
+
+printstyled("    gap < Δ/2 + 1\n", color=:light_green)
+for i in 1:1000
+  fs = rand(RandSeis.fs_vals)
+  nx = round(Int64, rand(1200:7200)*fs)
+  t = RandSeis.rand_t(fs, nx, 0)
+  δt = div(round(Int64, 1.0e6/fs), 2) + 1
+  gaps = t[2:end-1, 2]
+
+  if length(gaps) > 0
+    @test minimum(gaps) ≥ δt
+  end
+  @test minimum(diff(t[:,1])) > 0
+end
+
+printstyled("  randResp\n", color=:light_green)
+R = RandSeis.randResp(8)
+@test length(R.z) == length(R.p) == 8
+
 printstyled("  randSeis*\n", color=:light_green)
 for i = 1:10
   randSeisChannel()
   randSeisData()
   randSeisHdr()
+  randSeisSrc()
   randSeisEvent()
 end
 
-printstyled("  namestrip, namestrip!\n", color=:light_green)
+printstyled("  namestrip\n", color=:light_green)
 str = String(0x00:0xff)
 S = randSeisData(3)
 S.name[2] = str
