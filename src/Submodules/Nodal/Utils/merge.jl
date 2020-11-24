16c3ad2c00870b961fe50c903bd70e4178377432
Joshua <jpjones76@users.noreply.github.com>
2020-11-23 19:28:41 -0800

    disallow merge! on NodalData

--- src/Submodules/Nodal/Utils/merge.jl
+++ src/Submodules/Nodal/Utils/merge.jl
@@ -1,1 +1,3 @@
 merge_ext!(S::NodalData, Ω::Int64, rest::Array{Int64, 1}) = nothing
+
+merge!(S::NodalData) = error("NodalData cannot be merged!")
--- test/Nodal/test_processing.jl
+++ test/Nodal/test_processing.jl
@@ -4,19 +4,21 @@ fstr = path*"/SampleFiles/Nodal/Node1_UTC_20200307_170738.006.tdms"
 S1 = read_nodal("silixa", fstr)
 
 # these should all work
-for f in (:convert_seis!, :demean!, :detrend!, :merge!, :sync!, :taper!, :ungap!, :unscale!)
+for f in (:convert_seis!, :demean!, :detrend!, :sync!, :taper!, :ungap!, :unscale!)
   printstyled(string("    ", f, "\n"), color=:light_green)
   getfield(SeisIO, f)(S1)
 end
 
-# test resampling 
+@test_throws ErrorException merge!(S1)
+
+# test resampling
 printstyled(string("    ", "resample!", "\n"), color=:light_green)
-f0 = 500. 
+f0 = 500.
 S2 = resample(S1,f0)
 resample!(S1,f0)
 @test S2.data == S1.data
 
-# test filtering 
+# test filtering
 printstyled(string("    ", "filtfilt!", "\n"), color=:light_green)
 S2 = filtfilt(S1,rt="Bandpass",fl=100.,fh=200.)
 filtfilt!(S1,rt="Bandpass",fl=100.,fh=200.)
