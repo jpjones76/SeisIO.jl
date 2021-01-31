afa7a01167fb4463f3892f50a8cfa4deea22cc35
Joshua <jpjones76@users.noreply.github.com>
2021-01-30 18:26:26 -0800

    extend channel_match to GphysChannel; add cmatch_p!

--- src/Utils/channel_match.jl
+++ src/Utils/channel_match.jl
@@ -23,8 +23,40 @@ function channel_match( S::GphysData,
     end
   )
 end
+
 function channel_match( S::GphysData, C::GphysChannel )
   j = findid(S, C.id)
   j = channel_match( S, j, C.fs, C.gain, C.loc, C.resp, C.units)
   return j
+end
+
+function channel_match( C::GphysChannel, D::GphysChannel ; use_gain::Bool=true )
+  ff = use_gain ? (:id, :fs, :gain, :loc, :resp, :units) : (:id, :fs, :loc, :resp, :units)
+  return all([isequal(getfield(C, f), getfield(D, f)) for f in ff ])
+end
+
+# This will seek out a match and correct unset values if a partial match is found
+function cmatch_p!( C::GphysChannel, D::GphysChannel )
+  fs_match    = max(    C.fs == D.fs   ,    C.fs == default_fs   ,    D.fs == default_fs   )
+  gain_match  = max(  C.gain == D.gain ,  C.gain == default_gain ,  D.gain == default_gain )
+  loc_match   = max(   C.loc == D.loc  ,   C.loc == default_loc  ,   D.loc == default_loc  )
+  resp_match  = max(  C.resp == D.resp ,  C.resp == default_resp ,  D.resp == default_resp )
+  units_match = max( C.units == D.units, C.units == ""           , D.units == ""           )
+  m = min(C.id == D.id, fs_match, gain_match, loc_match, resp_match, units_match)
+  if m
+    # Fill any "unset" values in D from C
+    (    D.fs == default_fs   ) && (    D.fs = C.fs               )
+    (  D.gain == default_gain ) && (  D.gain = C.gain             )
+    (   D.loc == default_loc  ) && (   D.loc = deepcopy(C.loc)    )
+    (  D.resp == default_resp ) && (  D.resp = deepcopy(C.resp)   )
+    ( D.units == ""           ) && ( D.units = identity(C.units)  )
+
+    # Fill any "unset" values in C from D
+    (    C.fs == default_fs   ) && (    C.fs = D.fs               )
+    (  C.gain == default_gain ) && (  C.gain = D.gain             )
+    (   C.loc == default_loc  ) && (   C.loc = deepcopy(D.loc)    )
+    (  C.resp == default_resp ) && (  C.resp = deepcopy(D.resp)   )
+    ( C.units == ""           ) && ( C.units = identity(D.units)  )
+  end
+  return m
 end
--- test/Utils/test_chanutils.jl
+++ test/Utils/test_chanutils.jl
@@ -29,3 +29,88 @@ S.id[1] = "...0"
 filt_seis_chans!(cr, S)
 @test cr != c0
 @test cr == [2,3]
+
+printstyled("channel_match\n", color=:light_green)
+C = SeisChannel()
+D = SeisChannel()
+@test channel_match(C, D)
+
+C = randSeisChannel(s=true)
+D = deepcopy(C)
+C.gain = D.gain*0.5
+@test channel_match(C, D) == false
+@test channel_match(C, D, use_gain = false) == true
+
+C.gain = D.gain
+C.fs = D.fs*0.5
+@test channel_match(C, D) == false
+
+printstyled("cmatch_p!\n", color=:light_green)
+C = randSeisChannel(s=true)
+D = deepcopy(C)
+C0 = deepcopy(C)
+
+# Scenarios that must work:
+# C set, D unset
+D.fs = 0.0
+D.gain = 1.0
+D.loc = GeoLoc()
+D.resp = PZResp()
+D.units = ""
+m = cmatch_p!(C,D)
+@test m == true
+@test channel_match(C, D) == true
+
+# D set, C unset
+C.fs = 0.0
+C.gain = 1.0
+C.loc = GeoLoc()
+C.resp = PZResp()
+C.units = ""
+m = cmatch_p!(C,D)
+@test m == true
+@test channel_match(C, D) == true
+
+# Values must preserve those in C0
+@test channel_match(C, C0) == true
+
+# Scenarios that must fail
+C = randSeisChannel(s=true)
+C.loc = GeoLoc(lat = 48.79911, lon=-122.54064, el=45.1104)
+C.resp = PZResp(a0 = 223.43015f0, f0 = 2.0f0, p = ComplexF32.([-8.89+8.89im, 8.89-8.89im]))
+C0 = deepcopy(C)
+
+D = deepcopy(C0)
+while D.units == C.units
+  D.units = randstring()
+end
+@test cmatch_p!(C,D) == false
+@test C == C0
+
+D = deepcopy(C0)
+D.fs = 2.0*C.fs
+D0 = deepcopy(D)
+@test cmatch_p!(C,D) == false
+@test C == C0
+@test D == D0
+
+D = deepcopy(C0)
+D.gain = 2.0*C.gain
+D0 = deepcopy(D)
+@test cmatch_p!(C,D) == false
+@test C == C0
+@test D == D0
+
+D = deepcopy(C0)
+D.loc.lat = 89.1
+D0 = deepcopy(D)
+@test cmatch_p!(C,D) == false
+@test C == C0
+@test D == D0
+
+D = deepcopy(C0)
+D.resp.f0 = 1.0f0
+D0 = deepcopy(D)
+@test cmatch_p!(C,D) == false
+@test C == C0
+@test D == D0
