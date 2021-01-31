bdcf5f21d46ccfd4609196a7364d5a4ca59e49cc
Joshua <jpjones76@users.noreply.github.com>
2021-01-30 18:28:25 -0800

    mseis! extended to arbitrary GphysData, GphsChannel subtypes

--- src/Last/splat.jl
+++ src/Last/splat.jl
@@ -22,22 +22,27 @@ function SeisData(U...)
 end
 
 """
-    mseis!(S::SeisData, U::SeisData, ...)
+    mseis!(S::SeisData, U...)
 
-Merge multiple SeisData structures at once.
+Merge multiple SeisData structures at once. The first argument (merge target)
+must be a SeisData structure. Subsequent structures can be any type T <: Union{GphysData, GphysChannel, SeisEvent}.
+
+    mseis!(C::GphysChannel, U...)
+
+Merge all channels in U that match channel C into object C. To be merged, a
+channel must match on fields `:id`, `:fs`, `:loc`, `:resp`, `:units`.
 
 See also: `merge!`
 """
-function mseis!(S...)
-  U = Union{SeisData, SeisChannel, SeisEvent, EventTraceData, EventChannel}
+function mseis!(S1::SeisData, S...)
+  U = Union{GphysData, GphysChannel, SeisEvent}
   L = Int64(length(S))
-  (L < 2) && return
-  S1 = getindex(S, 1)
+  (L == 0) && return
   (typeof(S1) == SeisData) || error("Target must be type SeisData!")
-  for i = 2:L
+  for i in 1:L
     T = typeof(getindex(S, i))
     if (T <: U) == false
-      @warn(string("Object of incompatible type passed to wseis at ", i+1, "; skipped!"))
+      @warn(string("Object of incompatible type passed to mseis! at ", i+1, "; skipped!"))
       continue
     end
     if T == SeisData
@@ -54,4 +59,42 @@ function mseis!(S...)
   end
   merge!(S1)
   return S1
+end
+
+function mseis!(C::Y, S...) where Y<:GphysChannel
+  U = Union{GphysData, GphysChannel, SeisEvent}
+  for i in 1:length(S)
+    X = S[i]
+    T = typeof(X)
+
+    # Only merge compatible types
+    if (T <: U) == false
+      @warn(string("Object of incompatible type passed to mchan at ", i+1, "; skipped!"))
+      continue
+    end
+
+    # Only call merge_chan on channels with matching ID
+    if T <: GphysChannel
+      if channel_match(C, X, use_gain=false)
+        merge!(C, X)
+      end
+    elseif T <: GphysData
+      ID = getfield(X, :id)
+      for i in 1:X.n
+        D = getindex(X, i)
+        if channel_match(C, D, use_gain=false)
+          merge!(C, D)
+        end
+      end
+    elseif T == SeisEvent
+      X = X.data
+      for i in 1:X.n
+        D = getindex(X, i)
+        if channel_match(C, D, use_gain=false)
+          merge!(C, D)
+        end
+      end
+    end
+  end
+  return nothing
 end
